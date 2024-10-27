b43207a7a7b770acdc526cbc091d2e07dd548641
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-10-27 19:04:17 +0100

    Create dom.ts

---
+++ src/sanity/schemaTypes/dom.ts
@@ -0,0 +1,22 @@
+// cSpell:disable
+
+import { defineType, defineField } from 'sanity';
+
+export const dom = defineType({
+  name: 'dom',
+  title: 'Dom',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'powitanie',
+      title: 'Powitanie',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'wiadomosc',
+      title: 'Wiadomość',
+      type: 'text',
+    }),
+  ],
+});
