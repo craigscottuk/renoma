c3e7a7332a1e012edd8fabcaec3eeb50aa1b1ab5
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-10-27 19:04:17 +0100

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,29 @@
-import { type SchemaTypeDefinition } from 'sanity';
-// import { homeSchema } from './homeSchema';
-import { stronaGlowna } from './strona-glowna';
-import { settings } from './settings';
+// cSpell:disable
 
+import { type SchemaTypeDefinition } from 'sanity';
+
+// Import each schema file
+import { dom } from './dom';
+import { oNas } from './oNas';
+import { uslugi } from './uslugi';
+import { realizacje } from './realizacje';
+import { renomaLab } from './renomaLab';
+import { uczSieZNami } from './uczSieZNami';
+import { pracujZNami } from './pracujZNami';
+import { kontakt } from './kontakt';
+import { ustawienia } from './ustawienia';
+
+// Export schema configuration
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [stronaGlowna, settings],
+  types: [
+    dom,
+    oNas,
+    uslugi,
+    realizacje,
+    renomaLab,
+    uczSieZNami,
+    pracujZNami,
+    kontakt,
+    ustawienia,
+  ],
 };
