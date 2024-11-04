f8295e5e3552e643f3c78894f36bbf683ca3aaec
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-04 19:38:13 +0100

    Adding max-width-wrapper to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,11 @@
+// cSpell:disable
+
+import MaxWidhthWrapper from "./max-width-wrapper";
+
 export default function Footer() {
-  return <div className="bg-slate-850 bg-gray-600 text-white">Footer</div>;
+  return (
+    <MaxWidhthWrapper>
+      <div className="bg-gray-600 text-white">Footer</div>
+    </MaxWidhthWrapper>
+  );
 }
