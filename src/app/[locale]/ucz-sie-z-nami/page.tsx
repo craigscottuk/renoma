4bc1170366fb602b437b26d348d5da8fe9d3376c
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-04 09:16:36 +0100

    Adding routes and pages

---
+++ src/app/[locale]/ucz-sie-z-nami/page.tsx
@@ -0,0 +1,34 @@
+// cSpell:disable
+
+import { setRequestLocale } from "next-intl/server";
+import { client } from "@/sanity/client";
+
+const QUERY = `
+
+`;
+
+const OPTIONS = { next: { revalidate: 30 } };
+
+type Props = {
+  params: { locale: string };
+};
+
+interface Content {
+  powitanie: string;
+  wiadomosc: string;
+}
+
+export default async function UczSieZNami({ params: { locale } }: Props) {
+  // Set the locale for static generation
+  setRequestLocale(locale);
+
+  // Fetch localized content from Sanity using locale from params
+  const content = await client.fetch<Content>(QUERY, { locale }, OPTIONS);
+
+  return (
+    <main className="container mx-auto min-h-screen max-w-3xl p-8">
+      <h1 className="mb-8 text-4xl font-bold">UczSieZNami</h1>
+      <p>UczSieZNami</p>
+    </main>
+  );
+}
