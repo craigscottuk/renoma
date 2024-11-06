afe57ceac5ec4da75138fef4fb8d02c7f5d4fd69
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-06 15:44:01 +0100

    Spelling correction

--- src/components/max-width-wrapper.tsx
+++ src/components/max-width-wrapper.tsx
@@ -3,20 +3,18 @@
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
-const MaxWidhthWrapper = ({
+export default function MaxWidthWrapper({
   className,
   children,
 }: {
   className?: string;
   children: ReactNode;
-}) => {
+}) {
   return (
     <div
-      className={cn("mx-auto w-full max-w-[1680px] px-2.5 md:px-20", className)}
+      className={cn("mx-auto w-full max-w-[1680px] px-2.5 md:px-12", className)}
     >
       {children}
     </div>
   );
-};
-
-export default MaxWidhthWrapper;
+}
