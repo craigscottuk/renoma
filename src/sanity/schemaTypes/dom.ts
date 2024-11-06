91d7597fba2090712d70f774573759cdf69a71fc
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-06 17:53:04 +0100

    Update dom.ts

--- src/sanity/schemaTypes/dom.ts
+++ src/sanity/schemaTypes/dom.ts
@@ -1,25 +1,69 @@
 // cSpell:disable
-
-import { defineType, defineField } from 'sanity';
+import { defineType, defineField } from "sanity";
 
 export const dom = defineType({
-  name: 'dom',
-  title: 'Dom',
-  type: 'document',
+  name: "dom",
+  title: "Dom",
+  type: "document",
   options: {
     singleton: true,
   },
+  groups: [
+    {
+      name: "hero",
+      title: "Sekcja Powitalna",
+    },
+    {
+      name: "about",
+      title: "O Nas",
+    },
+    {
+      name: "services",
+      title: "Usługi",
+    },
+  ],
   fields: [
     defineField({
-      name: 'powitanie',
-      title: 'Powitanie',
-      type: 'internationalizedArrayString',
+      name: "powitanie",
+      title: "Powitanie",
+      group: "hero",
+      type: "internationalizedArrayString",
       validation: (Rule) => Rule.required(),
     }),
     defineField({
-      name: 'wiadomosc',
-      title: 'Wiadomość',
-      type: 'internationalizedArrayText',
+      name: "wiadomosc",
+      title: "Wiadomość",
+      group: "hero",
+      type: "internationalizedArrayText",
+    }),
+
+    // O Nas
+    defineField({
+      name: "oNasSectionLabel",
+      title: "Etykieta Sekcji O Nas",
+      group: "about",
+      type: "internationalizedArrayString",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "oNasSectionTitle",
+      title: "Tytuł Sekcji O Nas",
+      group: "about",
+      type: "internationalizedArrayString",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "oNasSectionDescription",
+      title: "Opis Sekcji O Nas",
+      group: "about",
+      type: "internationalizedArrayText",
+    }),
+    defineField({
+      name: "oNasSectionCTA",
+      title: "Przycisk Sekcji O Nas",
+      group: "about",
+      type: "internationalizedArrayString",
+      validation: (Rule) => Rule.required(),
     }),
   ],
 });
