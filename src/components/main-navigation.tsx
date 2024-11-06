1e6ddb01c861c969170eef7db2c81df5b95b1910
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-06 15:44:23 +0100

    Updating types

--- src/components/main-navigation.tsx
+++ src/components/main-navigation.tsx
@@ -3,19 +3,14 @@
 import { useTranslations } from "next-intl";
 import LocaleSwitcher from "./locale-switcher";
 import NavigationLinkItem from "./navigation-link-item";
-import MaxWidhthWrapper from "./max-width-wrapper";
+import MaxWidthWrapper from "./max-width-wrapper";
 import { Link } from "@/i18n/routing";
+import SideNavigation from "./side-navigation";
+import { RoutePaths } from "@/lib/types";
 
 type NavItem = {
   label: string;
-  link:
-    | "/o-nas"
-    | "/uslugi"
-    | "/realizacje"
-    | "/renoma-lab"
-    | "/ucz-sie-z-nami"
-    | "/pracuj-z-nami"
-    | "/kontakt";
+  link: RoutePaths;
 };
 
 export default function MainNavigation() {
@@ -24,43 +19,43 @@ export default function MainNavigation() {
   const navItems: NavItem[] = [
     {
       label: t("oNas.label"),
-      link: "/o-nas" as const,
+      link: "/o-nas",
     },
     {
       label: t("uslugi.label"),
-      link: "/uslugi" as const,
+      link: "/uslugi",
     },
     {
       label: t("realizacje.label"),
-      link: "/realizacje" as const,
+      link: "/realizacje",
     },
     {
       label: t("renomaLab.label"),
-      link: "/renoma-lab" as const,
+      link: "/renoma-lab",
     },
     {
       label: t("uczSieZNami.label"),
-      link: "/ucz-sie-z-nami" as const,
+      link: "/ucz-sie-z-nami",
     },
     {
       label: t("pracujZNami.label"),
-      link: "/pracuj-z-nami" as const,
+      link: "/pracuj-z-nami",
     },
     {
       label: t("kontakt.label"),
-      link: "/kontakt" as const,
+      link: "/kontakt",
     },
   ];
 
   return (
-    <MaxWidhthWrapper>
-      <header className="flex h-24 w-full items-end justify-between py-4">
+    <MaxWidthWrapper>
+      <header className="main-header flex h-24 w-full items-end justify-between py-4">
         {/* Logo */}
         <div className="">
           <Link href="/">
             <img
               className="h-[70px] w-auto"
-              src="/renoma-logo.svg"
+              src="/renoma-logo.png"
               alt="Renoma PKZ"
             />
           </Link>
@@ -79,7 +74,10 @@ export default function MainNavigation() {
             <LocaleSwitcher />
           </nav>
         </div>
+
+        {/* Side Navigation */}
+        <SideNavigation navItems={navItems} />
       </header>
-    </MaxWidhthWrapper>
+    </MaxWidthWrapper>
   );
 }
