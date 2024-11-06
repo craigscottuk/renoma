f70dd4cab0c9cc816b3f7a8e4d72683cc68c67ad
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-06 15:43:20 +0100

    Create types.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+// cSpell:disable
+export type RoutePaths =
+  | "/"
+  | "/o-nas"
+  | "/uslugi"
+  | "/realizacje"
+  | "/renoma-lab"
+  | "/ucz-sie-z-nami"
+  | "/pracuj-z-nami"
+  | "/kontakt";
