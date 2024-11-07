417e871600582ebbc48ca0a3e86b3b63a520140e
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-07 22:58:15 +0100

    Update navigation-link-item.tsx

--- src/components/navigation-link-item.tsx
+++ src/components/navigation-link-item.tsx
@@ -7,6 +7,7 @@ import { Link } from "@/i18n/routing";
 
 export default function NavigationLink({
   href,
+  className,
   ...rest
 }: ComponentProps<typeof Link>) {
   const selectedLayoutSegment = useSelectedLayoutSegment();
@@ -17,7 +18,8 @@ export default function NavigationLink({
     <Link
       aria-current={isActive ? "page" : undefined}
       className={clsx(
-        "inline-block px-3 py-3 uppercase leading-none tracking-[0.04em] antialiased transition-colors",
+        "inline-block py-0.5 uppercase leading-none tracking-[0.04em] antialiased transition-colors",
+        className,
         isActive ? "text-red-500" : "text-neutral-900 hover:text-gray-400",
       )}
       href={href}
