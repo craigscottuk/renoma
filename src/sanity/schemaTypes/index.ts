1d9813b030d984f5369fe460c94fcf446622821c
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-07 17:45:30 +0100

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,22 +1,28 @@
 // cSpell:disable
 
-import { type SchemaTypeDefinition } from 'sanity';
+import { type SchemaTypeDefinition } from "sanity";
 
 // Import each schema file
-import { dom } from './dom';
-import { oNas } from './oNas';
-import { uslugi } from './uslugi';
-import { realizacje } from './realizacje';
-import { renomaLab } from './renomaLab';
-import { uczSieZNami } from './uczSieZNami';
-import { pracujZNami } from './pracujZNami';
-import { kontakt } from './kontakt';
-import { ustawienia } from './ustawienia';
+import { dom, heroSection, aboutSection, servicesSection } from "./dom";
+import { oNas } from "./oNas";
+import { uslugi } from "./uslugi";
+import { realizacje } from "./realizacje";
+import { renomaLab } from "./renomaLab";
+import { uczSieZNami } from "./uczSieZNami";
+import { pracujZNami } from "./pracujZNami";
+import { kontakt } from "./kontakt";
+import { ustawienia } from "./ustawienia";
 
 // Export schema configuration
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
+    // dom
     dom,
+    heroSection,
+    aboutSection,
+    servicesSection,
+
+    // O nas
     oNas,
     uslugi,
     realizacje,
