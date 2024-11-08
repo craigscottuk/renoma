7a423d7b33c66b10de046cabf5033e82227144e5
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-08 23:02:32 +0100

    Update locale-switcher.tsx

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -2,12 +2,20 @@ import { useLocale, useTranslations } from "next-intl";
 import { routing } from "@/i18n/routing";
 import LocaleSwitcherSelect from "./locale-switcher-select";
 
-export default function LocaleSwitcher() {
+type LocaleSwitcherProps = {
+  className?: string;
+};
+
+export default function LocaleSwitcher({ className }: LocaleSwitcherProps) {
   const t = useTranslations("locale-switcher");
   const locale = useLocale();
 
   return (
-    <LocaleSwitcherSelect defaultValue={locale} label={t("label")}>
+    <LocaleSwitcherSelect
+      defaultValue={locale}
+      label={t("label")}
+      className={className}
+    >
       {routing.locales.map((cur) => (
         <option key={cur} value={cur}>
           {t("locale", { locale: cur })}
