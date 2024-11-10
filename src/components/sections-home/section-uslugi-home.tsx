fa4da57df8d168c7a5e8443163eb65dd744e0094
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-10 16:56:11 +0100

    Update section-uslugi-home.tsx

--- src/components/sections-home/section-uslugi-home.tsx
+++ src/components/sections-home/section-uslugi-home.tsx
@@ -1,13 +1,15 @@
 // cSpell:disable
-
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import CustomButton from "@/components/ui/custom-button";
+import SectionTitle from "@/components/section-title";
+import clsx from "clsx";
 
 interface SectionUslugiHomeProps {
   sectionLabel: string;
   sectionTitle: string;
   sectionDescription: string;
   sectionCTA: string;
+  paddingY?: string;
 }
 
 export default function SectionUslugiHome({
@@ -15,22 +17,25 @@ export default function SectionUslugiHome({
   sectionTitle,
   sectionDescription,
   sectionCTA,
+  paddingY = "py-16 md:py-44", // Default padding values
 }: SectionUslugiHomeProps) {
   return (
-    <section className="mx-auto bg-black py-16 text-white md:py-44">
+    <section className={clsx("mx-auto bg-black text-white", paddingY)}>
       <MaxWidthWrapper>
         <div className="grid items-center gap-12 md:grid-cols-2 md:gap-24">
           {/* Left Column */}
-          <div className="space-y-4 md:space-y-8">
-            <p className="text-sm uppercase tracking-wide">{sectionLabel}</p>
-            <h2 className="text-4xl font-light leading-tight md:text-6xl">
-              {sectionTitle}
-            </h2>
-          </div>
+          <SectionTitle
+            label={sectionLabel}
+            title={sectionTitle}
+            as="h2"
+            motionPreset="blur-left"
+            textColor="white"
+            textAlign="left"
+          />
 
           {/* Right Column */}
-          <div className="max-w-lg space-y-6 md:text-left">
-            <p className="max-w-prose text-base leading-relaxed">
+          <div className="max-w-lg md:text-left">
+            <p className="max-w-md text-base leading-relaxed">
               {sectionDescription}
             </p>
             <div>
