b8ebdf8d7ad8780248a0510e7803266d9abf04bd
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-10 16:56:10 +0100

    Update section-about.tsx

--- src/components/sections-home/section-about.tsx
+++ src/components/sections-home/section-about.tsx
@@ -1,51 +1,59 @@
 // cSpell:disable
-
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import CustomButton from "@/components/ui/custom-button";
+import SectionTitle from "@/components/section-title";
+import clsx from "clsx";
 
 interface SectionAboutProps {
   sectionLabel: string;
   sectionTitle: string;
   sectionDescription: string;
   sectionCTA: string;
+  paddingY?: string;
 }
 
-function SectionAbout({
+export default function SectionAbout({
   sectionLabel,
   sectionTitle,
   sectionDescription,
   sectionCTA,
+  paddingY = "py-16 md:py-44", // Default padding values
 }: SectionAboutProps) {
   return (
-    <section className="mx-auto bg-white py-16 md:py-44">
+    <section className={clsx("mx-auto bg-white", paddingY)}>
       <MaxWidthWrapper>
         <div className="grid gap-12 md:grid-cols-2 md:gap-24">
           {/* Right Column (Heading and Title) for Mobile, Hidden on Desktop */}
-          <div className="space-y-4 md:hidden">
-            <p className="text-left text-sm tracking-wide">{sectionLabel}</p>
-            <h2 className="text-6xl font-light leading-tight">
-              {sectionTitle}
-            </h2>
+          <div className="md:hidden">
+            <SectionTitle
+              label={sectionLabel}
+              title={sectionTitle}
+              as="h2"
+              motionPreset="blur-left"
+              textColor="black"
+            />
           </div>
 
           {/* Left Column (Text Content) */}
           <div className="flex h-full flex-col justify-center space-y-6">
-            <p className="max-w-md text-base leading-relaxed">
+            <p className="max-w-sm text-base leading-relaxed md:max-w-md">
               {sectionDescription}
             </p>
           </div>
 
           {/* Right Column (Heading, Title, and Button) for Desktop */}
-          <div className="hidden space-y-8 md:flex md:flex-col md:items-end">
-            <p className="text-sm uppercase tracking-wide">{sectionLabel}</p>
-            <h2 className="text-right text-4xl font-light leading-tight md:text-6xl">
-              {sectionTitle}
-            </h2>
-            <div>
-              <CustomButton variant="light" href="/o-nas">
-                {sectionCTA}
-              </CustomButton>
-            </div>
+          <div className="hidden md:flex md:flex-col md:items-end">
+            <SectionTitle
+              label={sectionLabel}
+              title={sectionTitle}
+              as="h2"
+              motionPreset="blur-left"
+              textColor="black"
+              textAlign="right"
+            />
+            <CustomButton variant="light" href="/o-nas">
+              {sectionCTA}
+            </CustomButton>
           </div>
 
           {/* Button for Mobile, Hidden on Desktop */}
@@ -59,5 +67,3 @@ function SectionAbout({
     </section>
   );
 }
-
-export default SectionAbout;
