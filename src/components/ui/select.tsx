20bf9d8e042613694b546ce1cc6edb4af34d668d
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-11 14:35:23 +0100

    Update select.tsx

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -20,6 +20,7 @@ const SelectTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "px-4 py-5 text-[1.1rem] leading-relaxed", // custom classes
       className,
     )}
     {...props}
@@ -119,6 +120,7 @@ const SelectItem = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "text-[1.1rem] leading-relaxed md:text-[1.1rem]", // custom classes
       className,
     )}
     {...props}
