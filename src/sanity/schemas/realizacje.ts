2a3ad4f1a21c59fbda42db0d12f6ee6b960112e4
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-12 19:21:36 +0100

    Update realizacje.ts

--- src/sanity/schemas/realizacje.ts
+++ src/sanity/schemas/realizacje.ts
@@ -1,27 +1,68 @@
 // cSpell:disable
-
-import { defineType, defineField } from 'sanity';
+import { defineType, defineField } from "sanity";
 
 export const realizacje = defineType({
-  name: 'realizacje',
-  title: 'Realizacje',
-  type: 'document',
+  name: "realizacje",
+  title: "Realizacje",
+  type: "document",
   fields: [
     defineField({
-      name: 'tytul',
-      title: 'Tytuł',
-      type: 'string',
-      validation: (Rule) => Rule.required(),
+      name: "title",
+      type: "string",
+      validation: (rule) => rule.required(),
     }),
     defineField({
-      name: 'opis',
-      title: 'Opis',
-      type: 'text',
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+      validation: (rule) =>
+        rule
+          .required()
+          .error("A slug is required to generate a page on the website"),
     }),
     defineField({
-      name: 'data',
-      title: 'Data',
-      type: 'date',
+      name: "summary",
+      type: "text",
+      rows: 3,
+      validation: (rule) =>
+        rule.max(200).warning("Summary should be less than 200 characters"),
+    }),
+    defineField({
+      name: "content",
+      type: "array",
+      title: "Content",
+      of: [
+        {
+          type: "block",
+        },
+        {
+          type: "image",
+        },
+      ],
+    }),
+
+    defineField({
+      name: "language",
+      type: "string",
+      readOnly: true,
     }),
   ],
+  preview: {
+    select: {
+      title: "title",
+      language: "language",
+      media: "image",
+    },
+    prepare(select) {
+      const { title, language, media } = select;
+
+      return {
+        title,
+        subtitle: language.toUpperCase(),
+        media,
+      };
+    },
+  },
 });
