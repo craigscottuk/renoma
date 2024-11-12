bb67f8f8d3ccad6bc97761c01d37f8e03af59984
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-12 15:18:30 +0100

    Create pracujZNami.ts

---
+++ src/sanity/schemas/pracujZNami.ts
@@ -0,0 +1,26 @@
+// cSpell:disable
+
+import { defineType, defineField } from 'sanity';
+
+export const pracujZNami = defineType({
+  name: 'pracujZNami',
+  title: 'Pracuj z Nami',
+  type: 'document',
+  options: {
+    singleton: true,
+  },
+  fields: [
+    defineField({
+      name: 'tytul',
+      title: 'Tytuł',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'ofertyPracy',
+      title: 'Oferty Pracy',
+      type: 'array',
+      of: [{ type: 'string' }],
+    }),
+  ],
+});
