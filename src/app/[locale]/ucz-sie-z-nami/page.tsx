fbfe58ddfe8cef4f1c023830e532754f3145e40a
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-12 15:17:42 +0100

    Update page.tsx

--- src/app/[locale]/ucz-sie-z-nami/page.tsx
+++ src/app/[locale]/ucz-sie-z-nami/page.tsx
@@ -1,21 +1,34 @@
 // cSpell:disable
-
 import { setRequestLocale } from "next-intl/server";
+import PageHeaderSection from "@/components/page-header-section";
 import { client } from "@/sanity/client";
 
-// const QUERY = `
+const QUERY = `
+{
+  "learnWithUsHeaderSection": *[_type == "learnWithUsHeaderSection"][0]{
+    "sectionLabel": coalesce(sectionLabel[_key == $locale][0].value, "Brak tłumaczenia"),
+    "sectionTitle": coalesce(sectionTitle[_key == $locale][0].value, "Brak tłumaczenia"),
+    "sectionDescription": coalesce(sectionDescription[_key == $locale][0].value, "Brak tłumaczenia"),
+    "headerImage": headerImage, // Fetch full image object with asset._ref
+    "headerImageAlt": coalesce(headerImage.alt[_key == $locale][0].value, "Brak tłumaczenia")
+  },
+}
+`;
 
-// `;
-
-// const OPTIONS = { next: { revalidate: 30 } };
+const OPTIONS = { next: { revalidate: 30 } };
 
 type Props = {
   params: { locale: string };
 };
 
 interface Content {
-  powitanie: string;
-  wiadomosc: string;
+  learnWithUsHeaderSection: {
+    sectionLabel: string;
+    sectionTitle: string;
+    sectionDescription: string;
+    headerImage?: string;
+    headerImageAlt?: string;
+  };
 }
 
 export default async function UczSieZNami({ params: { locale } }: Props) {
@@ -23,12 +36,17 @@ export default async function UczSieZNami({ params: { locale } }: Props) {
   setRequestLocale(locale);
 
   // Fetch localized content from Sanity using locale from params
-  // const content = await client.fetch<Content>(QUERY, { locale }, OPTIONS);
+  const content = await client.fetch<Content>(QUERY, { locale }, OPTIONS);
+
+  const { learnWithUsHeaderSection } = content;
 
   return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8">
-      <h1 className="mb-8 text-4xl font-bold">UczSieZNami</h1>
-      <p>UczSieZNami</p>
-    </main>
+    <PageHeaderSection
+      sectionLabel={learnWithUsHeaderSection.sectionLabel}
+      sectionTitle={learnWithUsHeaderSection.sectionTitle}
+      sectionDescription={learnWithUsHeaderSection.sectionDescription}
+      headerImage={learnWithUsHeaderSection.headerImage}
+      headerImageAlt={learnWithUsHeaderSection.headerImageAlt}
+    />
   );
 }
