442e8a482a92e67cd1592004b09b8bdef7c34c54
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-12 15:16:38 +0100

    Updating the schema folder structure

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import { visionTool } from '@sanity/vision';
-import { defineConfig } from 'sanity';
-import { structureTool } from 'sanity/structure';
-import { internationalizedArray } from 'sanity-plugin-internationalized-array';
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { internationalizedArray } from "sanity-plugin-internationalized-array";
 
-import { apiVersion, dataset, projectId } from './src/sanity/env';
-import { schema } from './src/sanity/schemaTypes';
-import { structure } from './src/sanity/structure';
-import { plPLLocale } from '@sanity/locale-pl-pl';
-import { singletonTools } from 'sanity-plugin-singleton-tools';
+import { apiVersion, dataset, projectId } from "./src/sanity/env";
+import { schema } from "./src/sanity/schemas";
+import { structure } from "./src/sanity/structure";
+import { plPLLocale } from "@sanity/locale-pl-pl";
+import { singletonTools } from "sanity-plugin-singleton-tools";
 
 export default defineConfig({
-  basePath: '/admin',
+  basePath: "/admin",
   projectId,
   dataset,
   schema,
@@ -25,12 +25,12 @@ export default defineConfig({
 
     internationalizedArray({
       languages: [
-        { id: 'pl', title: 'Polish' },
-        { id: 'en', title: 'English' },
-        { id: 'de', title: 'German' },
+        { id: "pl", title: "Polish" },
+        { id: "en", title: "English" },
+        { id: "de", title: "German" },
       ],
       // defaultLanguages: ['pl'], // Default to Polish for new documents
-      fieldTypes: ['string', 'text'],
+      fieldTypes: ["string", "text"],
       // buttonLocations: ['field'],
       // buttonAddAll: false,
     }),
