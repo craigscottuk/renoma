5671d2b222f4f8009c69c2cb478ec25048c0adc2
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-12 19:21:34 +0100

    Update page.tsx

--- src/app/[locale]/realizacje/page.tsx
+++ src/app/[locale]/realizacje/page.tsx
@@ -1,35 +1,22 @@
 // cSpell:disable
 import { setRequestLocale } from "next-intl/server";
-import PageHeaderSection from "@/components/page-header-section";
 import { client } from "@/sanity/client";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
 
 const QUERY = `
-{
-  "businessCasesHeaderSection": *[_type == "businessCasesHeaderSection"][0]{
-    "sectionLabel": coalesce(sectionLabel[_key == $locale][0].value, "Brak tłumaczenia"),
-    "sectionTitle": coalesce(sectionTitle[_key == $locale][0].value, "Brak tłumaczenia"),
-    "sectionDescription": coalesce(sectionDescription[_key == $locale][0].value, "Brak tłumaczenia"),
-    "headerImage": headerImage, // Fetch full image object with asset._ref
-    "headerImageAlt": coalesce(headerImage.alt[_key == $locale][0].value, "Brak tłumaczenia")
-  },
+*[_type == "realizacje" && language == $locale]{
+  title,
 }
-
 `;
 
-const OPTIONS = { next: { revalidate: 30 } };
+const OPTIONS = { next: { cache: "no-store" } };
 
 type Props = {
   params: { locale: string };
 };
 
 interface Content {
-  businessCasesHeaderSection: {
-    sectionLabel: string;
-    sectionTitle: string;
-    sectionDescription: string;
-    headerImage?: string;
-    headerImageAlt?: string;
-  };
+  title: string;
 }
 
 export default async function Realizacje({ params: { locale } }: Props) {
@@ -37,17 +24,22 @@ export default async function Realizacje({ params: { locale } }: Props) {
   setRequestLocale(locale);
 
   // Fetch localized content from Sanity using locale from params
-  const content = await client.fetch<Content>(QUERY, { locale }, OPTIONS);
+  const content = await client.fetch<Content[]>(QUERY, { locale });
 
-  const { businessCasesHeaderSection } = content;
+  console.log("project content: ", content);
 
   return (
-    <PageHeaderSection
-      sectionLabel={businessCasesHeaderSection.sectionLabel}
-      sectionTitle={businessCasesHeaderSection.sectionTitle}
-      sectionDescription={businessCasesHeaderSection.sectionDescription}
-      headerImage={businessCasesHeaderSection.headerImage}
-      headerImageAlt={businessCasesHeaderSection.headerImageAlt}
-    />
+    <>
+      <div className="mt-48">
+        <MaxWidthWrapper>
+          <div>Test</div>
+          <div className="text-black">
+            {content.map((item, index) => (
+              <h2 key={index}>{item.title}</h2>
+            ))}
+          </div>
+        </MaxWidthWrapper>
+      </div>
+    </>
   );
 }
