36df21ee0c988f7e79cf378df37a960fe4d448ff
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-12 15:58:33 +0100

    Create privacy.ts

---
+++ src/sanity/schemas/privacy.ts
@@ -0,0 +1,89 @@
+// cSpell:disable
+import { defineType, defineField } from "sanity";
+
+export const privacy = defineType({
+  name: "privacy",
+  title: "Polityka Prywatności",
+  type: "document",
+  options: {
+    singleton: true,
+  },
+  fields: [
+    {
+      name: "placeholder",
+      title: "Zastępczy tekst",
+      type: "string",
+    },
+  ],
+});
+
+// Privacy Policy page header section
+export const privacyHeaderSection = defineType({
+  name: "privacyHeaderSection",
+  title: "Nagłówek strony polityki prywatności",
+  type: "document",
+  options: { singleton: true },
+  groups: [
+    {
+      name: "etykietaSekcji",
+      title: "Etykieta sekcji",
+    },
+    {
+      name: "tytulSekcji",
+      title: "Tytuł sekcji",
+    },
+    {
+      name: "opisSekcji",
+      title: "Opis sekcji",
+    },
+    {
+      name: "obrazSekcji",
+      title: "Obraz sekcji",
+    },
+  ],
+  fields: [
+    defineField({
+      name: "sectionLabel",
+      title: "Etykieta sekcji",
+      description: "Krótki tekst nad tytułem, np. 'POLITYKA PRYWATNOŚCI'.",
+      type: "internationalizedArrayString",
+      group: "etykietaSekcji",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "sectionTitle",
+      title: "Tytuł sekcji",
+      description:
+        "Główny tytuł sekcji na stronie polityki prywatności, np. 'Polityka prywatności'.",
+      type: "internationalizedArrayString",
+      group: "tytulSekcji",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "sectionDescription",
+      title: "Opis sekcji",
+      description:
+        "Tekst wprowadzający do strony polityki prywatności, zawierający ogólne informacje na temat ochrony danych.",
+      type: "internationalizedArrayText",
+      group: "opisSekcji",
+    }),
+    defineField({
+      name: "headerImage",
+      title: "Obraz nagłówka",
+      description: "Obraz wyświetlany w nagłówku strony polityki prywatności.",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      group: "obrazSekcji",
+    }),
+    defineField({
+      name: "headerImageAlt",
+      title: "Alternatywny tekst obrazu nagłówka",
+      description:
+        "Tekst alternatywny dla obrazu nagłówka, np. 'Ilustracja symbolizująca ochronę prywatności'.",
+      type: "internationalizedArrayString",
+      group: "obrazSekcji",
+    }),
+  ],
+});
