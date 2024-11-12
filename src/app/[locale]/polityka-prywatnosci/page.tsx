4b5c9546fc682a7f51505141148368683d9560f2
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-12 15:17:38 +0100

    Update page.tsx

--- src/app/[locale]/polityka-prywatnosci/page.tsx
+++ src/app/[locale]/polityka-prywatnosci/page.tsx
@@ -1,21 +1,35 @@
 // cSpell:disable
-
 import { setRequestLocale } from "next-intl/server";
-// import { client } from "@/sanity/client";
+import PageHeaderSection from "@/components/page-header-section";
+import { client } from "@/sanity/client";
 
-// const QUERY = `
+const QUERY = `
+{
+  "privacyHeaderSection": *[_type == "privacyHeaderSection"][0]{
+    "sectionLabel": coalesce(sectionLabel[_key == $locale][0].value, "Brak tłumaczenia"),
+    "sectionTitle": coalesce(sectionTitle[_key == $locale][0].value, "Brak tłumaczenia"),
+    "sectionDescription": coalesce(sectionDescription[_key == $locale][0].value, "Brak tłumaczenia"),
+    "headerImage": headerImage, // Fetch full image object with asset._ref
+    "headerImageAlt": coalesce(headerImage.alt[_key == $locale][0].value, "Brak tłumaczenia")
+  },
+}
 
-// `;
+`;
 
-// const OPTIONS = { next: { revalidate: 30 } };
+const OPTIONS = { next: { revalidate: 30 } };
 
 type Props = {
   params: { locale: string };
 };
 
 interface Content {
-  powitanie: string;
-  wiadomosc: string;
+  privacyHeaderSection: {
+    sectionLabel: string;
+    sectionTitle: string;
+    sectionDescription: string;
+    headerImage?: string;
+    headerImageAlt?: string;
+  };
 }
 
 export default async function PolitykaPrywatnosci({
@@ -25,12 +39,17 @@ export default async function PolitykaPrywatnosci({
   setRequestLocale(locale);
 
   // Fetch localized content from Sanity using locale from params
-  // const content = await client.fetch<Content>(QUERY, { locale }, OPTIONS);
+  const content = await client.fetch<Content>(QUERY, { locale }, OPTIONS);
+
+  const { privacyHeaderSection } = content;
 
   return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8">
-      <h1 className="mb-8 text-4xl font-bold">Polityka Prywatnosci</h1>
-      <p>Polityka Prywatnosci</p>
-    </main>
+    <PageHeaderSection
+      sectionLabel={privacyHeaderSection.sectionLabel}
+      sectionTitle={privacyHeaderSection.sectionTitle}
+      sectionDescription={privacyHeaderSection.sectionDescription}
+      headerImage={privacyHeaderSection.headerImage}
+      headerImageAlt={privacyHeaderSection.headerImageAlt}
+    />
   );
 }
