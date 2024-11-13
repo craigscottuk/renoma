dde16f6e6b4abc8865d4beadde68f30fc4a5c4c7
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-13 18:03:58 +0100

    Update the app/[locale]/realizacje/page.tsx

--- src/app/[locale]/realizacje/page.tsx
+++ src/app/[locale]/realizacje/page.tsx
@@ -1,45 +1,81 @@
 // cSpell:disable
+// app/[locale]/realizacje/page.tsx
 import { setRequestLocale } from "next-intl/server";
 import { client } from "@/sanity/client";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { Link } from "@/i18n/routing";
+import PageHeaderSection from "@/components/page-header-section";
 
 const QUERY = `
-*[_type == "realizacje" && language == $locale]{
-  title,
+{
+  "projects": *[_type == "wpisRealizacji" && language == $locale]{
+    title,
+    slug,
+    language,
+},
+  "realizacjeHeaderSection": *[_type == "realizacjeHeaderSection"][0]{
+    "sectionLabel": coalesce(sectionLabel[_key == $locale][0].value, "Brak tłumaczenia"),
+    "sectionTitle": coalesce(sectionTitle[_key == $locale][0].value, "Brak tłumaczenia"),
+    "sectionDescription": coalesce(sectionDescription[_key == $locale][0].value, "Brak tłumaczenia"),
+    "headerImage": headerImage,
+    "headerImageAlt": coalesce(headerImage.alt[_key == $locale][0].value, "Brak tłumaczenia")
+  }
 }
 `;
 
-const OPTIONS = { next: { cache: "no-store" } };
+const OPTIONS = { next: { revalidate: 30 } };
 
 type Props = {
   params: { locale: string };
 };
 
-interface Content {
+interface Projects {
   title: string;
+  slug: {
+    current: string;
+  };
 }
 
 export default async function Realizacje({ params: { locale } }: Props) {
-  // Set the locale for static generation
   setRequestLocale(locale);
 
-  // Fetch localized content from Sanity using locale from params
-  const content = await client.fetch<Content[]>(QUERY, { locale });
+  const { projects, realizacjeHeaderSection } = await client.fetch(QUERY, {
+    locale,
+  });
 
-  console.log("project content: ", content);
+  console.log(projects);
 
   return (
     <>
-      <div className="mt-48">
+      {/* Page Header Section */}
+      <PageHeaderSection
+        sectionLabel={realizacjeHeaderSection.sectionLabel}
+        sectionTitle={realizacjeHeaderSection.sectionTitle}
+        sectionDescription={realizacjeHeaderSection.sectionDescription}
+        headerImage={realizacjeHeaderSection.headerImage}
+        headerImageAlt={realizacjeHeaderSection.headerImageAlt}
+      />
+
+      <section className="py-12 md:py-24">
         <MaxWidthWrapper>
-          <div>Test</div>
-          <div className="text-black">
-            {content.map((item, index) => (
-              <h2 key={index}>{item.title}</h2>
+          <h1 className="mb-8 text-4xl">Projects</h1>
+          <ul className="flex flex-col gap-y-4">
+            {/* use slug or id instead of index */}
+            {projects.map((project: Projects, index: number) => (
+              <li className="hover:underline" key={index}>
+                <Link
+                  href={{
+                    pathname: "/realizacje/[slug]",
+                    params: { slug: project.slug.current },
+                  }}
+                >
+                  <h2 className="text-xl">{project.title}</h2>
+                </Link>
+              </li>
             ))}
-          </div>
+          </ul>
         </MaxWidthWrapper>
-      </div>
+      </section>
     </>
   );
 }
