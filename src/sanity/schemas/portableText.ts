e1d332398c31f92cbbc05ec4fedf201bce19e2dd
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-17 22:37:12 +0100

    Update portableText.ts

--- src/sanity/schemas/portableText.ts
+++ src/sanity/schemas/portableText.ts
@@ -1,23 +1,20 @@
 import { defineType, defineField } from "sanity";
 
-export const portableText = defineType({
-  name: "portableText",
-  title: "Portable Text",
+export const basicText = defineType({
+  name: "basicText",
+  title: "Basic Text",
   type: "array",
   of: [
     defineField({
       name: "block",
       type: "block",
-      styles: [
-        { title: "Heading 2", value: "h2" },
-        { title: "Heading 3", value: "h3" },
+      styles: [{ title: "Normal", value: "normal" }],
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" },
       ],
       marks: {
-        decorators: [
-          { title: "Strong", value: "strong" },
-          { title: "Emphasis", value: "em" },
-          { title: "Code", value: "code" },
-        ],
+        decorators: [{ title: "Bold", value: "strong" }],
         annotations: [
           defineField({
             name: "link",
@@ -33,6 +30,60 @@ export const portableText = defineType({
           }),
         ],
       },
+    }),
+  ],
+});
+
+export const portableTextWithImage = defineType({
+  name: "portableTextWithImage",
+  title: "Portable Text with Image",
+  type: "array",
+  of: [
+    defineField({
+      name: "block",
+      type: "block",
+      styles: [
+        { title: "Normal", value: "normal" },
+        { title: "Heading 3", value: "h3" },
+        { title: "Heading 4", value: "h4" },
+      ],
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" },
+      ],
+      marks: {
+        decorators: [{ title: "Bold", value: "strong" }],
+        annotations: [
+          defineField({
+            name: "link",
+            type: "object",
+            title: "URL",
+            fields: [
+              {
+                title: "URL",
+                name: "href",
+                type: "url",
+              },
+            ],
+          }),
+        ],
+      },
+    }),
+    defineField({
+      name: "image",
+      type: "image",
+      title: "Image",
+      options: {
+        hotspot: true, // Enables image cropping
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+          description: "Important for accessibility and SEO.",
+        },
+      ],
     }),
   ],
 });
