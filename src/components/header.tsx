1dbeb86bd89ca101446c20f1e2620a57a5151eac
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-20 22:25:50 +0100

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,22 +7,20 @@ import { Link } from "@/i18n/routing";
 
 export default function Header() {
   return (
-    <header className="fixed left-0 z-30 h-24 w-[100vw] bg-white">
+    <header className="fixed left-0 z-30 h-20 w-[100vw] bg-white md:h-24">
       <MaxWidthWrapper className="h-full">
         <div className="flex h-full w-full items-center">
           {/* Logo */}
           <Link href="/">
-            <Image
-              className="h-[65px]"
+            <img
+              className="h-[50px] md:h-[65px]"
               src="/renoma-logo.svg"
               alt="Renoma PKZ"
-              width={180}
-              height={65}
             />
           </Link>
 
           {/* Nav Items */}
-          <div className="ml-auto mt-7 flex items-end">
+          <div className="ml-auto flex h-full items-center">
             <MainNavigation />
             {/* Locale Switcher */}
             <LocaleSwitcher className="hidden xl:flex" />
