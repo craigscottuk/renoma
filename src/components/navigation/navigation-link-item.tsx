4de1040144d2d3fd8adfd12779c2a6f99ca4f5c8
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-11-28 18:44:14 +0100

    Update navigation-link-item.tsx

--- src/components/navigation/navigation-link-item.tsx
+++ src/components/navigation/navigation-link-item.tsx
@@ -25,7 +25,7 @@ export default function NavigationLink({
     <Link
       aria-current={isActive ? "page" : undefined}
       className={clsx(
-        "group inline-block py-0.5 uppercase leading-none tracking-[0.04em] antialiased transition-colors duration-300 ease-in-out",
+        "group motion-preset-blur-up inline-block py-0.5 uppercase leading-none tracking-[0.04em] antialiased transition-colors duration-300 ease-in-out",
         className,
         isActive
           ? "text-[#765911] underline decoration-[#765911] decoration-1 underline-offset-8"
