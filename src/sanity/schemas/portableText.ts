48794cb4315e187805fc9adaa1fe83378cd646d3
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-03 21:57:11 +0100

    Update portableText.ts

--- src/sanity/schemas/portableText.ts
+++ src/sanity/schemas/portableText.ts
@@ -87,3 +87,40 @@ export const portableTextWithImage = defineType({
     }),
   ],
 });
+export const portableTextWithHeadings = defineType({
+  name: "portableTextWithHeadings",
+  title: "Portable Text with Image",
+  type: "array",
+  of: [
+    defineField({
+      name: "block",
+      type: "block",
+      styles: [
+        { title: "Normal", value: "normal" },
+        { title: "Heading 3", value: "h3" },
+        { title: "Heading 4", value: "h4" },
+      ],
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" },
+      ],
+      marks: {
+        decorators: [{ title: "Bold", value: "strong" }],
+        annotations: [
+          defineField({
+            name: "link",
+            type: "object",
+            title: "URL",
+            fields: [
+              {
+                title: "URL",
+                name: "href",
+                type: "url",
+              },
+            ],
+          }),
+        ],
+      },
+    }),
+  ],
+});
