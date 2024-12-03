bde2aef5cf2bc777334eb63264fed7dc1d38986b
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-03 21:57:03 +0100

    Update error.tsx

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 
-import { useTranslations } from 'next-intl';
-import { useEffect } from 'react';
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { useTranslations } from "next-intl";
+import { useEffect } from "react";
 
 type Props = {
   error: Error;
@@ -9,26 +10,30 @@ type Props = {
 };
 
 export default function Error({ error, reset }: Props) {
-  const t = useTranslations('error-page');
+  const t = useTranslations("error-page");
 
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      {t.rich('description', {
-        p: (chunks) => <p className='mt-4'>{chunks}</p>,
-        retry: (chunks) => (
-          <button
-            className='text-white underline underline-offset-2'
-            onClick={reset}
-            type='button'
-          >
-            {chunks}
-          </button>
-        ),
-      })}
-    </div>
+    <MaxWidthWrapper>
+      <div className="mt-40 min-h-[50Vh] text-center">
+        <h1 className="text-4xl font-bold text-black">{t("title")}</h1>
+        <div className="mt-8">
+          {t.rich("description", {
+            p: (chunks) => <p className="mt-4 text-lg">{chunks}</p>,
+            retry: (chunks) => (
+              <a
+                className="mt-6 cursor-pointer text-black underline underline-offset-2"
+                onClick={reset}
+              >
+                {chunks}
+              </a>
+            ),
+          })}
+        </div>
+      </div>
+    </MaxWidthWrapper>
   );
 }
