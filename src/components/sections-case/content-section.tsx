08145fbc63b3ad42eb7793cf7d7334b6215b4f87
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-04 17:16:35 +0100

    Update content-section.tsx

--- src/components/sections-case/content-section.tsx
+++ src/components/sections-case/content-section.tsx
@@ -1,64 +1,155 @@
 import React from "react";
 import MaxWidthWrapper from "../max-width-wrapper";
 import ImageCarousel from "../ImageCarousel";
+import { PortableText } from "@portabletext/react";
+import { portableTextComponents } from "../../lib/portableTextComponents";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { CaseStudySectionContent } from "@/types";
 
-export default function ContentSection() {
+export default function ContentSection({
+  sectionContent,
+}: {
+  sectionContent: CaseStudySectionContent[];
+}) {
   return (
     <div>
-      {/* 1 */}
-      {/* Text on the left and photo on the right */}
+      {sectionContent?.map(
+        (section: CaseStudySectionContent, index: number) => {
+          switch (section._type) {
+            case "textAndImageGallery":
+              return (
+                <MaxWidthWrapper
+                  key={index}
+                  className={`flex flex-col lg:flex-row lg:gap-24`}
+                >
+                  <div className="flex-1 lg:w-1/2">
+                    <PortableText
+                      value={section.text || []}
+                      components={portableTextComponents}
+                    />
+                  </div>
+                  <div className="flex flex-col lg:w-1/2 lg:items-start">
+                    <div className="w-full">
+                      <ImageCarousel
+                        images={
+                          section.images?.map(
+                            (img: {
+                              asset: SanityImageSource | string;
+                              caption: string;
+                            }) => ({
+                              src:
+                                typeof img.asset === "string"
+                                  ? img.asset
+                                  : urlFor(img.asset),
+                              caption: img.caption,
+                            }),
+                          ) || []
+                        }
+                        aspectRatio={section.aspectRatio}
+                      />
+                    </div>
+                  </div>
+                </MaxWidthWrapper>
+              );
+            case "textAndText":
+              return (
+                <MaxWidthWrapper
+                  key={index}
+                  className={`flex flex-col lg:flex-row lg:gap-24`}
+                >
+                  <div className="flex-1 lg:w-1/2">
+                    <PortableText
+                      value={section.text1 || []}
+                      components={portableTextComponents}
+                    />
+                  </div>
+                  <div className="flex flex-col lg:w-1/2 lg:items-start">
+                    <PortableText
+                      value={section.text2 || []}
+                      components={portableTextComponents}
+                    />
+                  </div>
+                </MaxWidthWrapper>
+              );
+            case "imageGalleryAndImageGallery":
+              return (
+                <MaxWidthWrapper
+                  key={index}
+                  className={`flex flex-col lg:flex-row lg:gap-24`}
+                >
+                  <div className="flex-1 lg:w-1/2">
+                    <ImageCarousel
+                      images={
+                        section.images1?.map(
+                          (img: {
+                            asset: SanityImageSource | string;
+                            caption: string;
+                          }) => ({
+                            src:
+                              typeof img.asset === "string"
+                                ? img.asset
+                                : urlFor(img.asset),
+                            caption: img.caption,
+                          }),
+                        ) || []
+                      }
+                      aspectRatio={section.aspectRatio1}
+                    />
+                  </div>
+                  <div className="flex flex-col lg:w-1/2 lg:items-start">
+                    <ImageCarousel
+                      images={
+                        section.images2?.map(
+                          (img: {
+                            asset: SanityImageSource | string;
+                            caption: string;
+                          }) => ({
+                            src:
+                              typeof img.asset === "string"
+                                ? img.asset
+                                : urlFor(img.asset),
+                            caption: img.caption,
+                          }),
+                        ) || []
+                      }
+                      aspectRatio={section.aspectRatio2}
+                    />
+                  </div>
+                </MaxWidthWrapper>
+              );
+            case "fullWidthImage":
+              const imageUrl =
+                typeof section.image?.asset === "string"
+                  ? section.image.asset
+                  : section.image?.asset
+                    ? urlFor(section.image.asset)
+                    : undefined;
 
-      <MaxWidthWrapper>
-        <h2 className="mb-8 font-bolder text-4xl">Aktualny Stan</h2>
-      </MaxWidthWrapper>
-      <MaxWidthWrapper className={`flex flex-col lg:flex-row lg:gap-8`}>
-        {/* Empty column */}
-
-        <div className="flex-1 lg:w-1/2">This is column 1</div>
-
-        {/* TDImahe carosuel  */}
-        <div className="flex flex-col lg:w-1/2 lg:items-start">
-          <ImageCarousel
-            images={[
-              { src: "/placeholder.png", caption: "Caption 1" },
-              { src: "/placeholder.png", caption: "Caption 2" },
-            ]}
-          />
-        </div>
-      </MaxWidthWrapper>
-
-      {/* 2 */}
-      {/* Photo on the left and text on the right */}
-      <MaxWidthWrapper className={`flex flex-col lg:flex-row lg:gap-8`}>
-        {/* Empty column */}
-        <div className="flex-1 lg:w-1/2">This is column 1</div>
-        {/* Details column */}
-        <div className="flex flex-col lg:w-1/2 lg:items-start">
-          This is column 2
-        </div>
-      </MaxWidthWrapper>
-
-      {/* 3 */}
-      {/* Text on the left and text on the right */}
-      <MaxWidthWrapper className={`flex flex-col lg:flex-row lg:gap-8`}>
-        {/* Empty column */}
-        <div className="flex-1 lg:w-1/2">This is column 1</div>
-        {/* Details column */}
-        <div className="flex flex-col lg:w-1/2 lg:items-start">
-          This is column 2
-        </div>
-      </MaxWidthWrapper>
-
-      {/* 4 */}
-      {/* Text on the left and text on the right */}
-      <MaxWidthWrapper className={`flex flex-col lg:flex-row lg:gap-8`}>
-        {/* Empty column */}
-        <div className="flex-1 lg:w-1/2">This is column 1</div>
-        {/* Details column */}
-        <div className="flex flex-col lg:w-1/2 lg:items-start">
-          This is column 2
-        </div>
-      </MaxWidthWrapper>
+              return (
+                <MaxWidthWrapper
+                  key={index}
+                  className={`flex flex-col lg:flex-row lg:gap-24`}
+                >
+                  <div className="flex-1 lg:w-full">
+                    {imageUrl && (
+                      <Image
+                        src={imageUrl}
+                        alt={section.image?.caption || ""}
+                        className="w-full"
+                        width={1200}
+                        height={1200}
+                      />
+                    )}
+                  </div>
+                </MaxWidthWrapper>
+              );
+            default:
+              return null;
+          }
+        },
+      )}
     </div>
   );
 }
