4f110ea3742ff95d21004b668f13752a21c042ca
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-06 10:37:48 +0100

    Update details.tsx

--- src/components/sections-case/details.tsx
+++ src/components/sections-case/details.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { useState } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronUp,
+  ArrowDownRight,
+  ArrowUpLeft,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import MaxWidthWrapper from "../max-width-wrapper";
-import { Detail } from "@/types";
+import clsx from "clsx";
+
+type Detail = {
+  label: string;
+  value: string | string[];
+};
 
 export default function ProjectDetailsSection({
   className,
@@ -16,7 +26,9 @@ export default function ProjectDetailsSection({
 }) {
   const [isExpanded, setIsExpanded] = useState(false);
 
-  const nonNullDetails = details.filter((detail) => detail.value !== null);
+  const nonNullDetails = details.filter(
+    (detail: Detail) => detail.value !== null,
+  );
   const visibleDetails = isExpanded
     ? nonNullDetails
     : nonNullDetails.slice(0, 4);
@@ -26,67 +38,59 @@ export default function ProjectDetailsSection({
   }
 
   return (
-    <section className={`relative mx-auto bg-white py-12 ${className}`}>
-      <MaxWidthWrapper>
-        <Separator className="mb-12" />
-      </MaxWidthWrapper>
-
+    <section className={clsx("relative mx-auto bg-white py-12", className)}>
       <MaxWidthWrapper className={`flex flex-col lg:flex-row lg:gap-8`}>
         {/* Empty column */}
         <div className="flex-1 lg:w-1/2"></div>
         {/* Details column */}
         <div className="flex flex-col lg:w-1/2 lg:items-start">
-          <p className="mb-6 font-medium text-[base] text-muted-foreground">
-            ⊙ Szczegóły projektu
-          </p>
           <div className="grid w-full grid-cols-1 gap-6">
-            {visibleDetails.map((detail, index) => (
-              <div key={index}>
-                <div className="grid grid-cols-2 gap-12">
-                  <div className="font-bolder text-[base]">{detail.label}</div>
-                  <div className="text-[base]">
-                    {Array.isArray(detail.value) ? (
-                      detail.value.length > 1 ? (
-                        <ul className="list-disc space-y-1">
-                          {detail.value.map((item, i) => (
-                            <li key={i}>{item}</li>
-                          ))}
-                        </ul>
+            {visibleDetails.map((detail: Detail, index: number) => (
+              <>
+                <div key={index}>
+                  <div className="grid grid-cols-2 gap-12 text-[1.1rem]">
+                    <div className="font-bolder">{detail.label}</div>
+                    <div>
+                      {Array.isArray(detail.value) ? (
+                        detail.value.length > 1 ? (
+                          <ul className="list-disc space-y-1">
+                            {detail.value.map((item: string, i: number) => (
+                              <li key={i}>{item}</li>
+                            ))}
+                          </ul>
+                        ) : (
+                          detail.value[0]
+                        )
                       ) : (
-                        detail.value[0]
-                      )
-                    ) : (
-                      detail.value
-                    )}
+                        detail.value
+                      )}
+                    </div>
                   </div>
                 </div>
-                <Separator className="my-3" />
-              </div>
+                <Separator className="my-0" />
+              </>
             ))}
           </div>
           {nonNullDetails.length > 4 && (
             <Button
               variant="link"
-              className="px-0 font-bolder text-[base]"
+              className="mt-6 p-0 px-0 font-bolder text-[1.1rem]"
               onClick={() => setIsExpanded(!isExpanded)}
             >
               {isExpanded ? (
                 <>
                   Pokaż mniej
-                  <ChevronUp className="ml-2 h-4 w-4" />
+                  <ChevronUp className="h-6 w-6" />
                 </>
               ) : (
                 <>
                   Pokaż więcej
-                  <ChevronDown className="ml-2 h-4 w-4" />
+                  <ChevronDown className="h-6 w-6" />
                 </>
               )}
             </Button>
           )}
         </div>
-      </MaxWidthWrapper>
-      <MaxWidthWrapper>
-        <Separator className="mt-12" />
       </MaxWidthWrapper>
     </section>
   );
