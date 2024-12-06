37aef959616cfc40b13f67c16a4d50d88af1ed9a
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-06 19:23:37 +0100

    Create wpisUslugi.ts

---
+++ src/sanity/schemas/wpisUslugi.ts
@@ -0,0 +1,200 @@
+// cSpell:disable
+import { defineType, defineField } from "sanity";
+
+export const wpisUslugi = defineType({
+  name: "wpisUslugi",
+  title: "Wpis Usługi",
+  type: "document",
+  groups: [
+    {
+      name: "home",
+      title: "Homepage",
+    },
+    {
+      name: "services",
+      title: "Services page",
+    },
+  ],
+  fields: [
+    defineField({
+      name: "title",
+      title: "Service Title",
+      description:
+        "Title of the service, e.g. 'Badania Konserwatorskie i Ekspertyzy'.",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "slug",
+      type: "slug",
+      title: "Slug",
+      description:
+        "Unikalny identyfikator URL projektu, generowany na podstawie tytułu, np. 'konserwacja-baszty'.",
+      options: {
+        source: "title",
+      },
+      validation: (rule) =>
+        rule
+          .required()
+          .error("Slug jest wymagany do wygenerowania strony projektu."),
+    }),
+
+    defineField({
+      name: "summary",
+      title: "Podsumowanie",
+      description:
+        "Krótki opis projektu, maksymalnie 500 znaków. Używane jako wprowadzenie do studium przypadku w sekcji nagłówkowej strony.",
+      type: "text",
+      rows: 8,
+      validation: (rule) =>
+        rule
+          .required()
+          .error(
+            "Podsumowanie jest wymagane i powinno mieć mniej niż 500 znaków.",
+          ),
+    }),
+
+    defineField({
+      name: "headerImage",
+      title: "Nagłówek",
+      type: "object",
+      group: "caseStudyHeader",
+      fields: [
+        {
+          name: "image",
+          title: "Obraz nagłówka",
+          type: "image",
+        },
+        {
+          name: "imageAlt",
+          title: "Alternatywny tekst obrazu nagłówka",
+          type: "string",
+        },
+      ],
+    }),
+
+    // ================================
+    // Szczegóły Projektu
+    // ================================
+
+    defineField({
+      name: "details",
+      title: "Szczegóły Projektu",
+      type: "object",
+      description:
+        "Zawiera szczegółowe informacje o projekcie, takie jak lokalizacja, status, czas trwania, typ obiektu, role oraz zakres prac.",
+      group: "projectDetails",
+      fields: [
+        {
+          name: "lokalizacja",
+          title: "Lokalizacja",
+          type: "string",
+          description:
+            "Podaj lokalizację projektu, np. 'Wzgórze Katedralne we Fromborku'.",
+        },
+        {
+          name: "status",
+          title: "Status",
+          type: "string",
+          description:
+            "Podaj aktualny status projektu, np. 'W trakcie realizacji' lub 'Zakończony'.",
+        },
+        {
+          name: "czasTrwania",
+          title: "Czas trwania",
+          type: "string",
+          description:
+            "Podaj czas trwania projektu, np. '12 miesięcy (styczeń 2024 - grudzień 2024)'.",
+        },
+        {
+          name: "typObiektu",
+          title: "Typ Obiektu",
+          type: "string",
+          description:
+            "Podaj typ obiektu, np. 'Zabytek architektury obronnej'.",
+        },
+        {
+          name: "rola",
+          title: "Rola",
+          type: "array",
+          of: [{ type: "string" }],
+          description:
+            "Podaj role w projekcie, np. 'Autor programu prac konserwatorskich.', 'Generalny wykonawca prac konserwatorskich i restauratorskich oraz budowlanych'.",
+        },
+        {
+          name: "zakresPrac",
+          title: "Zakres Prac",
+          type: "array",
+          of: [{ type: "string" }],
+          description:
+            "Podaj zakres prac, np. 'Stabilizacja konstrukcji budynku.', 'Remont więźby dachowej.', 'Wymiana pokrycia dachowego.'.",
+        },
+      ],
+    }),
+
+    // ================================
+    // 01. Rys Historyczny (lub inny)
+    // ================================
+
+    defineField({
+      name: "sectionOne",
+      title: "01. Rys Historyczny (lub inny)",
+      type: "object",
+      group: "contentSection",
+      fields: [
+        {
+          name: "title",
+          title: "Tytuł Sekcji",
+          type: "string",
+          initialValue: "Rys historyczny",
+        },
+        {
+          name: "content",
+          title: "Treść Sekcji",
+          type: "sectionContent",
+        },
+      ],
+    }),
+
+    // ================================
+    // 02. Stan zachowania (lub inny)
+    // ================================
+
+    defineField({
+      name: "sectionTwo",
+      title: "02. Stan zachowania (lub inny)",
+      type: "object",
+      group: "contentSection",
+      fields: [
+        {
+          name: "title",
+          title: "Tytuł Sekcji",
+          type: "string",
+          initialValue: "Stan zachowania",
+        },
+        {
+          name: "content",
+          title: "Treść Sekcji",
+          type: "sectionContent",
+        },
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+      language: "language",
+      media: "image",
+    },
+    prepare(select) {
+      const { title, language, media } = select;
+
+      return {
+        title,
+        subtitle: language.toUpperCase(),
+        media,
+      };
+    },
+  },
+});
