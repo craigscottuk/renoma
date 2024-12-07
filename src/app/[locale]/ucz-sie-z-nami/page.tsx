ae6d8e76fb4d85c06d93d8e47fb2904690ee8db7
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-07 22:17:42 +0100

    Update page.tsx

--- src/app/[locale]/ucz-sie-z-nami/page.tsx
+++ src/app/[locale]/ucz-sie-z-nami/page.tsx
@@ -2,7 +2,9 @@
 import { setRequestLocale } from "next-intl/server";
 import PageHeaderSection from "@/components/page-header-section";
 import { client } from "@/sanity/client";
-import CaseContentOne from "@/components/sections-case/case-concept-one";
+import WhatWeOffer from "@/components/sections-learn-with-us/what-we-offer";
+import WhoWeAreLookingFor from "@/components/sections-learn-with-us/who-we-are-looking-for";
+import { PortableTextBlock } from "next-sanity";
 
 const QUERY = `
 {
@@ -13,8 +15,23 @@ const QUERY = `
     "headerImage": headerImage, // Fetch full image object with asset._ref
     "headerImageAlt": coalesce(headerImage.alt[_key == $locale][0].value, "Brak tłumaczenia")
   },
-
-  "wpisRealizacji": *[_type == "wpisRealizacji" && slug == 'konserwacja-baszty-ferbera']{title,}
+  "whatWeOfferSection": *[_type == "whatWeOfferSection"][0]{
+    "sectionTitle": coalesce(sectionTitle[_key == $locale][0].value, "Brak tłumaczenia"),
+    "offers": offers[]{
+      "title": coalesce(title[_key == $locale][0].value, "Brak tłumaczenia"),
+      "description": coalesce(description[_key == $locale][0].value, "Brak tłumaczenia")
+    }
+  },
+  "whoWeAreLookingForSection": *[_type == "whoWeAreLookingForSection"][0]{
+    "sectionTitle": coalesce(sectionTitle[_key == $locale][0].value, "Brak tłumaczenia"),
+    "criteria": select(
+      defined(criteria[$locale]) => criteria[$locale],
+      "Brak tłumaczenia"
+    ),
+    "image": image,
+    "imageAlt": coalesce(imageAlt[_key == $locale][0].value, "Brak tłumaczenia"),
+    "applyButtonText": coalesce(applyButtonText[_key == $locale][0].value, "Brak tłumaczenia")
+  }
 }
 `;
 
@@ -32,6 +49,17 @@ interface Content {
     headerImage?: string;
     headerImageAlt?: string;
   };
+  whatWeOfferSection: {
+    sectionTitle: string;
+    offers: { title: string; description: string }[];
+  };
+  whoWeAreLookingForSection: {
+    sectionTitle: string;
+    criteria: PortableTextBlock[];
+    image: string;
+    imageAlt: string;
+    applyButtonText: string;
+  };
 }
 
 export default async function UczSieZNami({ params: { locale } }: Props) {
@@ -41,19 +69,38 @@ export default async function UczSieZNami({ params: { locale } }: Props) {
   // Fetch localized content from Sanity using locale from params
   const content = await client.fetch<Content>(QUERY, { locale }, OPTIONS);
 
-  const { learnWithUsHeaderSection } = content;
+  const {
+    learnWithUsHeaderSection,
+    whatWeOfferSection,
+    whoWeAreLookingForSection,
+  } = content;
 
   return (
     <>
-      {/* <PageHeaderSection
+      <PageHeaderSection
         sectionLabel={learnWithUsHeaderSection.sectionLabel}
         sectionTitle={learnWithUsHeaderSection.sectionTitle}
         sectionDescription={learnWithUsHeaderSection.sectionDescription}
         headerImage={learnWithUsHeaderSection.headerImage}
         headerImageAlt={learnWithUsHeaderSection.headerImageAlt}
-      /> */}
+      />
 
-      <CaseContentOne />
+      {/* What we offer */}
+      <WhatWeOffer
+        sectionTitle={whatWeOfferSection.sectionTitle}
+        offers={whatWeOfferSection.offers}
+        paddingY="py-20 md:py-48"
+      />
+
+      {/* Who we are looking for */}
+      <WhoWeAreLookingFor
+        sectionTitle={whoWeAreLookingForSection.sectionTitle}
+        criteria={whoWeAreLookingForSection.criteria}
+        image={whoWeAreLookingForSection.image}
+        imageAlt={whoWeAreLookingForSection.imageAlt}
+        applyButtonText={whoWeAreLookingForSection.applyButtonText}
+        paddingY="py-20 md:py-48"
+      />
     </>
   );
 }
