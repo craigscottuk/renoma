40e2545877956b7e40c568e30ee3ae4b921c09f3
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-08 21:00:54 +0100

    Update page-header-section.tsx

--- src/components/page-header-section.tsx
+++ src/components/page-header-section.tsx
@@ -5,108 +5,136 @@ import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import SectionTitle from "@/components/section-title";
 import SectionDescription from "@/components/section-description";
 import MaxWidthWrapper from "./max-width-wrapper";
+import clsx from "clsx";
 
 interface PageHeaderSectionProps {
-  sectionLabel: string;
-  sectionTitle: string;
-  sectionDescription: string;
+  label: string;
+  title: string;
+  description: string;
   sectionButton?: string;
-  headerImage?: SanityImageSource | string;
-  headerImageAlt?: string;
+  image?: SanityImageSource | string;
+  imageAlt?: string;
+  headerFullWidth?: boolean;
 }
 
 export default function PageHeaderSection({
-  sectionLabel,
-  sectionTitle,
-  sectionDescription,
+  label,
+  title,
+  description,
   sectionButton,
-  headerImage,
-  headerImageAlt,
+  image,
+  imageAlt,
+  headerFullWidth = true,
 }: PageHeaderSectionProps) {
   // Generate the header image URL from Sanity or use the raw string URL
-  const headerImageUrl =
-    typeof headerImage === "string"
-      ? headerImage
-      : headerImage
-        ? urlFor(headerImage, 1200)
-        : undefined;
+  const imageUrl =
+    typeof image === "string"
+      ? image
+      : image
+        ? urlFor(image, 1200)
+        : "/fallback-image.svg";
 
   return (
-    <section className="relative mx-auto mt-24 bg-white py-12 lg:py-24">
-      {/* Render a top image strip for small screens */}
-      {headerImageUrl && (
-        <MaxWidthWrapper className="relative -mt-12 mb-4 block h-44 w-full lg:hidden">
+    <>
+      {/* Full-width header image */}
+      {headerFullWidth && imageUrl && (
+        <div className="relative mt-24 h-80 w-full">
           <Image
-            src={headerImageUrl}
-            alt={headerImageAlt || "Header image"} // Use alt text or fallback
+            src={imageUrl || "/fallback-image.svg"}
+            alt={imageAlt || "Obraz nagłówka"}
             fill
             style={{
-              objectFit: "cover", // Ensures the image covers the area
-              objectPosition: "center", // Centers the visible part of the image
+              objectFit: "cover",
+              objectPosition: "center",
             }}
+            loading="lazy"
           />
-        </MaxWidthWrapper>
+        </div>
       )}
 
-      {/* Main content layout */}
-      <MaxWidthWrapper
-        className={`flex flex-col ${
-          headerImageUrl ? "lg:flex-row lg:items-start" : "lg:flex-row lg:gap-8"
-        }`}
+      {/* Text - Section title and description */}
+      <section
+        className={clsx(
+          "relative mx-auto mt-24 bg-white py-12 lg:py-16",
+          headerFullWidth ? "lg:mt-0 lg:pt-10" : "",
+        )}
       >
-        {headerImageUrl ? (
-          // Stacked layout for content with an image on large screens
-          <div className="flex-1">
-            <SectionTitle
-              label={sectionLabel}
-              title={sectionTitle}
-              as="h1"
-              motionPreset="blur-left"
-              textColor="black"
+        {/* Small-screen header image strip */}
+        {!headerFullWidth && imageUrl && (
+          <MaxWidthWrapper className="relative -mt-12 mb-4 block h-44 w-full lg:hidden">
+            <Image
+              src={imageUrl}
+              alt={imageAlt || "Header image"}
+              fill
+              style={{
+                objectFit: "cover",
+                objectPosition: "center",
+              }}
             />
-            <SectionDescription
-              description={sectionDescription}
-              marginTop={true}
-              textStyle="text-balance"
-            />
-          </div>
-        ) : (
-          // Two-column layout for large screens without an image
-          <>
+          </MaxWidthWrapper>
+        )}
+
+        {/* Section content */}
+        <MaxWidthWrapper
+          className={clsx("flex flex-col", {
+            "lg:flex-row lg:items-start": imageUrl && !headerFullWidth,
+            "lg:flex-row lg:gap-8": !imageUrl || headerFullWidth,
+          })}
+        >
+          {imageUrl && !headerFullWidth ? (
+            // Stacked layout for content with an image on the right on large screens
             <div className="flex-1">
               <SectionTitle
-                label={sectionLabel}
-                title={sectionTitle}
+                label={label}
+                title={title}
                 as="h1"
                 motionPreset="blur-left"
                 textColor="black"
               />
-            </div>
-            <div className="flex flex-col items-end md:max-w-[29rem]">
               <SectionDescription
-                description={sectionDescription}
+                description={description}
                 marginTop={true}
                 textStyle="text-balance"
               />
             </div>
-          </>
-        )}
+          ) : (
+            // Two-column layout for large screens without an image
+            <>
+              <div className="flex-1">
+                <SectionTitle
+                  label={label}
+                  title={title}
+                  as="h1"
+                  motionPreset="blur-left"
+                  textColor="black"
+                />
+              </div>
+              <div className="flex flex-col items-end md:max-w-[29rem]">
+                <SectionDescription
+                  description={description}
+                  marginTop={true}
+                  textStyle="text-balance"
+                />
+              </div>
+            </>
+          )}
 
-        {/* Large-screen image rendering */}
-        {headerImageUrl && (
-          <div className="relative hidden lg:-mb-80 lg:-mt-12 lg:ml-auto lg:block lg:h-[620px] lg:w-[520px]">
-            <Image
-              src={headerImageUrl}
-              alt={headerImageAlt || "Header image"}
-              fill
-              style={{
-                objectFit: "cover", // Ensures the image maintains proportions
-                objectPosition: "center", // Centers the image content
-              }}
-            />
-          </div>
-        )}
-      </MaxWidthWrapper>
-    </section>
+          {/* Large-screen layout with image on the right */}
+          {imageUrl && !headerFullWidth && (
+            <div className="relative hidden lg:-mb-80 lg:-mt-16 lg:ml-auto lg:block lg:h-[580px] lg:w-[500px]">
+              <Image
+                src={imageUrl}
+                alt={imageAlt || "Obraz nagłówka"}
+                fill
+                style={{
+                  objectFit: "cover",
+                  objectPosition: "center",
+                }}
+              />
+            </div>
+          )}
+        </MaxWidthWrapper>
+      </section>
+    </>
   );
 }
