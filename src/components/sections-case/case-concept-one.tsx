f1747a4017ff3a46e757ce2c6ec438c7fe9f23f3
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-08 21:00:55 +0100

    Update case-concept-one.tsx

--- src/components/sections-case/case-concept-one.tsx
+++ src/components/sections-case/case-concept-one.tsx
@@ -8,10 +8,10 @@ export default function CaseContentOne() {
       {/* Header */}
 
       <CaseStudyHeaderSection
-        sectionLabel={`Studium Przypadku`}
-        sectionTitle={`Konserwacja Baszty Ferbera`}
-        sectionDescription={`Wybudowana w pierwszej połowie XVI wieku basteja (Baszta Ferbera) stanowi ważny element wschodniego odcinka obwarowań Wzgórza Katedralnego we Fromborku. Projekt konserwatorski przewidywał między innymi stabilizację konstrukcji budynku oraz usunięcie przyczyn osłabienia i destrukcji materii zabytkowej. W ciągu 12 miesięcy zrealizowano wszystkie cele i założenia projektu zapobiegając dalszemu niszczeniu budowli, utrwalając jej wartość historyczną i doprowadzając do stanu technicznego umożliwiającego jej użytkowanie.`}
-        headerImage={"/basteja.jpg"}
+        label={`Studium Przypadku`}
+        title={`Konserwacja Baszty Ferbera`}
+        description={`Wybudowana w pierwszej połowie XVI wieku basteja (Baszta Ferbera) stanowi ważny element wschodniego odcinka obwarowań Wzgórza Katedralnego we Fromborku. Projekt konserwatorski przewidywał między innymi stabilizację konstrukcji budynku oraz usunięcie przyczyn osłabienia i destrukcji materii zabytkowej. W ciągu 12 miesięcy zrealizowano wszystkie cele i założenia projektu zapobiegając dalszemu niszczeniu budowli, utrwalając jej wartość historyczną i doprowadzając do stanu technicznego umożliwiającego jej użytkowanie.`}
+        image={"/basteja.jpg"}
       />
       <ProjectDetailsSection />
       <ContentSection />
