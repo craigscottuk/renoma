d633c88cfef2a8cdd1e8f82556304ac5bb680c03
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-10 18:01:07 +0100

    Create privacy.tsx

---
+++ src/app/[locale]/polityka-prywatnosci/privacy.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { PortableText, PortableTextBlock } from "@portabletext/react";
+import { portableTextComponents } from "@/lib/portableTextComponents";
+
+interface PrivacyProps {
+  content: PortableTextBlock[];
+  paddingY?: string;
+}
+
+export default function Privacy({ content, paddingY }: PrivacyProps) {
+  return (
+    <section className={clsx("mx-auto bg-white text-black/90", paddingY)}>
+      <MaxWidthWrapper>
+        <PortableText value={content} components={portableTextComponents} />
+      </MaxWidthWrapper>
+    </section>
+  );
+}
