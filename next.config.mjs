ea2877462b4c34ece69f00c8b7081126292a8fad
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-15 20:13:55 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const nextConfig = {
         pathname: "/**", // Match all paths on this domain
       },
     ],
+    unoptimized: true, // Disable image optimization
   },
 
   async redirects() {
