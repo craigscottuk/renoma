76ae48db7c814b35730f5c63466ed4091f74dac3
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-15 15:40:10 +0100

    Create about-lab.tsx

---
+++ src/app/[locale]/renoma-lab/about-lab.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import SectionTitle from "@/components/section-title";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { portableTextComponents } from "@/lib/portableTextComponents";
+import { PortableText, PortableTextBlock } from "@portabletext/react";
+
+interface AboutLabProps {
+  title: string;
+  text: PortableTextBlock[];
+  paddingY: string;
+}
+
+export function AboutLab({ title, text, paddingY }: AboutLabProps) {
+  return (
+    <section className={clsx("mx-auto bg-white text-black/90", paddingY)}>
+      <MaxWidthWrapper>
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:gap-24">
+          <SectionTitle title={title} textColor="black" />
+          <div className="lg:col-span-2 lg:col-start-2 lg:columns-2 lg:gap-8">
+            <PortableText value={text} components={portableTextComponents} />
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </section>
+  );
+}
