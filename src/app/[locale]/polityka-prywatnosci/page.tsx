84380bf8fec676ac918ece32cd0890fc5fb82bcc
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-15 15:40:06 +0100

    Update page.tsx

--- src/app/[locale]/polityka-prywatnosci/page.tsx
+++ src/app/[locale]/polityka-prywatnosci/page.tsx
@@ -40,7 +40,7 @@ interface Content {
     description: string;
     image?: string;
     imageAlt?: string;
-    imageLayout?: "fullWidth" | "portraitRight";
+    imageLayout?: "fullWidthAbove" | "fullWidthBelow" | "portraitRight";
     backgroundColor?: "black" | "white";
   };
   privacyText: {
