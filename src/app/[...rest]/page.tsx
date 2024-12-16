ad7bf4527512cbbe8cf379d89b995093d0d0f059
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-16 13:01:08 +0100

    Update page.tsx

--- src/app/[...rest]/page.tsx
+++ src/app/[...rest]/page.tsx
@@ -1,4 +1,6 @@
-import { notFound } from 'next/navigation';
+import { notFound } from "next/navigation";
+
+export const dynamic = "force-dynamic";
 
 export default function CatchAllPage() {
   notFound();
