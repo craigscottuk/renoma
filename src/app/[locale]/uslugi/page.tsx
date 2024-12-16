e90ee3af81a4c96f5b22fd71307f1801c4eaa7e1
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-16 15:39:30 +0100

    Update page.tsx

--- src/app/[locale]/uslugi/page.tsx
+++ src/app/[locale]/uslugi/page.tsx
@@ -3,6 +3,7 @@ import { setRequestLocale } from "next-intl/server";
 import PageHeader from "@/components/page-header";
 import { client } from "@/sanity/client";
 import ServicesList from "@/app/[locale]/uslugi/services-list";
+// import { type SanityDocument } from "next-sanity";
 
 const QUERY = `
 {
@@ -57,7 +58,9 @@ const QUERY = `
 }
 `;
 
-const OPTIONS = { next: { revalidate: false } };
+// const OPTIONS = { next: { revalidate: false } };
+// const OPTIONS = { cache: "force-cache" };
+const OPTIONS = { next: { revalidate: 86400 } }; // Persistent caching
 
 type Props = {
   params: { locale: string };
