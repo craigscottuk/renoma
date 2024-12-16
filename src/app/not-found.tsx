1b74478e956ddc3f9e7716125254ad80144dad8b
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-16 13:01:12 +0100

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,11 @@
-import BaseLayout from '@/components/base-layout';
-import NotFoundPage from '@/components/not-found-page';
-import { routing } from '@/i18n/routing';
+import BaseLayout from "@/components/base-layout";
+import NotFoundPage from "@/components/not-found-page";
+import { routing } from "@/i18n/routing";
 
 // This page renders when a route like `/unknown.txt` is requested.
 // In this case, the layout at `app/[locale]/layout.tsx` receives
 // an invalid value as the `[locale]` param and calls `notFound()`.
+export const dynamic = "force-dynamic";
 
 export default function GlobalNotFound() {
   return (
