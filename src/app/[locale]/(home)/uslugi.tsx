6540cf93181af42d90ebb7b3b2bbaec138ed6184
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-18 19:50:09 +0100

    Update uslugi.tsx

--- src/app/[locale]/(home)/uslugi.tsx
+++ src/app/[locale]/(home)/uslugi.tsx
@@ -4,16 +4,9 @@ import SectionTitle from "@/components/section-title";
 import CustomButton from "@/components/ui/custom-button";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import SectionDescription from "@/components/section-description";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import AnimatedLink from "@/components/animated-link";
+import ServicesCards from "./services-cards"; // Add this import
 
-interface SectionUslugiHomeProps {
+export interface SectionUslugiHomeProps {
   label: string;
   title: string;
   description: string;
@@ -53,7 +46,7 @@ export default function SectionUslugiHome({
   servicesGroup,
 }: SectionUslugiHomeProps) {
   return (
-    <section className={clsx("mx-auto bg-zinc-900 text-white", paddingY)}>
+    <section className={clsx("mx-auto bg-zinc-900 text-zinc-100", paddingY)}>
       <MaxWidthWrapper>
         <div className="relative grid items-center gap-6 md:grid-cols-2 md:gap-24">
           {/* Left Column */}
@@ -64,6 +57,8 @@ export default function SectionUslugiHome({
             motionPreset="blur-left"
             textColor="white"
             textAlign="left"
+            animateOnView={true}
+            animationDirection="left"
           />
 
           {/* Right Column */}
@@ -73,9 +68,16 @@ export default function SectionUslugiHome({
                 description={description}
                 textAlign="left"
                 textColor="white"
+                animateOnView={true}
+                animationDirection="right"
               />
               <div className="w-full text-left">
-                <CustomButton variant="dark" href="/uslugi">
+                <CustomButton
+                  animateOnView={true}
+                  animationDirection="left"
+                  variant="dark"
+                  href="/uslugi"
+                >
                   {sectionCTA}
                 </CustomButton>
               </div>
@@ -86,55 +88,5 @@ export default function SectionUslugiHome({
         <ServicesCards servicesGroup={servicesGroup} />
       </MaxWidthWrapper>
     </section>
-  );
-}
-
-function ServicesCards({
-  servicesGroup,
-}: {
-  servicesGroup: SectionUslugiHomeProps["servicesGroup"];
-}) {
-  const serviceGroups = [
-    servicesGroup.serviceGroupOne,
-    servicesGroup.serviceGroupTwo,
-    servicesGroup.serviceGroupThree,
-  ];
-
-  return (
-    <div className="mt-14 grid grid-cols-1 gap-6 md:mt-20 md:grid-cols-3">
-      {serviceGroups.map((group, index) => (
-        <Card key={index} className="border border-white/40 bg-[#09090B]">
-          <CardHeader className="text-white">
-            <CardTitle className="font-regular text-[1.6rem] text-white/95 md:text-[1.8rem]">
-              {group.title}
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-col space-y-4 text-white/80">
-            <Accordion type="multiple" className="w-full">
-              {group.services.map((item, itemIndex) => (
-                <AccordionItem
-                  className="border-white/60"
-                  key={itemIndex}
-                  value={`item-${itemIndex}`}
-                >
-                  <AccordionTrigger className="text-left text-[1.2rem]">
-                    {item.title}
-                  </AccordionTrigger>
-                  <AccordionContent className="text-balance text-[1.1rem]">
-                    <p className="mb-10">{item.shortDescription}</p>
-                    <AnimatedLink
-                      href={`/uslugi#${item.title.toLowerCase().replace(/\s+/g, "-")}`}
-                      variant="dark"
-                    >
-                      Więcej szczegółów
-                    </AnimatedLink>
-                  </AccordionContent>
-                </AccordionItem>
-              ))}
-            </Accordion>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
   );
 }
