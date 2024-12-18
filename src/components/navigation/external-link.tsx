15b1197a75bf6d1550f459b80fdee607a5120ff6
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-18 19:50:17 +0100

    Update external-link.tsx

--- src/components/navigation/external-link.tsx
+++ src/components/navigation/external-link.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function ExternalLink({ href, title }: Props) {
   return (
     <a className="" href={href} rel="noreferrer" target="_blank">
-      <p className="mt-1 text-[1.1rem] text-white">
+      <p className="mt-1 text-[1.1rem] text-zinc-100">
         {title} <span className="ml-2 inline-block">→</span>
       </p>
     </a>
