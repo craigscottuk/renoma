8007fa0849f149932e46ebee9df1c9cec56f574a
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-18 19:50:07 +0100

    Update hero.tsx

--- src/app/[locale]/(home)/hero.tsx
+++ src/app/[locale]/(home)/hero.tsx
@@ -2,6 +2,7 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import CustomButton from "@/components/ui/custom-button";
 import SectionTitle from "@/components/section-title";
+import MuxPlayer from "@mux/mux-player-react";
 
 interface HeroSectionProps {
   title: string;
@@ -12,7 +13,7 @@ export default function HeroSection({ title, sectionCTA }: HeroSectionProps) {
   return (
     <div className="relative mt-20 min-h-svh md:mt-24 md:min-h-[calc(100vh-96px)]">
       {/* Background Video */}
-      {/* <MuxPlayer
+      <MuxPlayer
         streamType="on-demand"
         playbackId="4W02Db7nBRvUHNxB6qsJZroV9900uPi2gm5c3qoiZFJ38"
         metadataVideoTitle="Renoma"
@@ -20,10 +21,10 @@ export default function HeroSection({ title, sectionCTA }: HeroSectionProps) {
         loop={true}
         muted={true}
         autoPlay={true}
-      /> */}
+      />
 
       {/* Overlay for better readability */}
-      <div className="absolute inset-0 bg-zinc-950 bg-opacity-50"></div>
+      <div className="absolute inset-0 bg-zinc-900 bg-opacity-50"></div>
 
       {/* Hero Content */}
       <div className="absolute inset-0 flex items-center">
