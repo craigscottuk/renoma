a436d89270cf60111e1f105ce77998149c300a4b
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-22 00:52:17 +0100

    Update project-card.tsx

--- src/app/[locale]/realizacje/project-card.tsx
+++ src/app/[locale]/realizacje/project-card.tsx
@@ -2,6 +2,14 @@
 import Image from "next/image";
 import { ArrowRight } from "lucide-react";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import clsx from "clsx";
 
 interface ProjectCardProps {
   projectCardData: {
@@ -12,17 +20,60 @@ interface ProjectCardProps {
     imageUrl: string;
     slug: string;
   }[];
+  paddingY: string;
+  colorScheme: keyof typeof cardColorSchemes;
 }
 
-export default function ProjectCard({ projectCardData }: ProjectCardProps) {
+const cardColorSchemes: Record<
+  string,
+  {
+    section: string;
+    card: string;
+    cardContent: string;
+    sectionTitle: "white" | "black";
+  }
+> = {
+  zincDark: {
+    section: "bg-zinc-900 text-zinc-50",
+    card: "border-zinc-700 bg-zinc-900 text-zinc-50",
+    cardContent: "text-zinc-100",
+    sectionTitle: "white",
+  },
+  zincLight: {
+    section: "text-zinc-950 bg-zinc-200",
+    card: "border-zinc-300 bg-zinc-100",
+    cardContent: "text-zinc-700",
+    sectionTitle: "black",
+  },
+  goldDark: {
+    section: "bg-gold-950 text-gold-50",
+    card: "border-gold-700 bg-gold-900 text-gold-50",
+    cardContent: "text-gold-200",
+    sectionTitle: "white",
+  },
+};
+
+export default function ProjectCard({
+  projectCardData,
+  paddingY,
+  colorScheme = "zincLight",
+}: ProjectCardProps) {
+  const selectedColorScheme =
+    cardColorSchemes[colorScheme] || cardColorSchemes.zincDark;
+
   return (
-    <section className="bg-white py-32">
+    <section
+      className={clsx("bg-white", paddingY, selectedColorScheme.section)}
+    >
       <MaxWidthWrapper>
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           {projectCardData.map((project, index) => (
-            <div
+            <Card
               key={index}
-              className="flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
+              className={clsx(
+                "flex flex-col overflow-hidden",
+                selectedColorScheme.card,
+              )}
             >
               <div className="relative h-48 lg:h-64">
                 <a href={`/realizacje/${project.slug}`}>
@@ -34,28 +85,41 @@ export default function ProjectCard({ projectCardData }: ProjectCardProps) {
                   />
                 </a>
               </div>
-              <div className="flex flex-grow flex-col p-6">
-                <h2 className="mb-2 text-xl font-semibold">{project.title}</h2>
+              <CardHeader>
+                <div className="flex items-center space-x-2">
+                  <CardTitle className="text-[1.6rem] leading-tight tracking-[-0.015em]">
+                    {project.title}
+                  </CardTitle>
+                </div>
+              </CardHeader>
+              <CardContent
+                className={clsx(
+                  "text-[1.1rem]",
+                  selectedColorScheme.cardContent,
+                )}
+              >
                 <div className="mb-4">
-                  <p className="text-sm text-gray-600">
+                  <p className="text-sm text-muted-foreground">
                     <strong>Lokalizacja:</strong> {project.location}
                   </p>
-                  <p className="text-sm text-gray-600">
+                  <p className="text-sm text-muted-foreground">
                     <strong>Czas trwania:</strong> {project.timeframe}
                   </p>
                 </div>
-                <p className="mb-4 flex-grow text-gray-700">
+                <p className="text-sm text-muted-foreground">
                   {project.description}
                 </p>
+              </CardContent>
+              <CardFooter>
                 <a
                   href={`/realizacje/${project.slug}`}
-                  className="mt-auto inline-flex items-center text-blue-600 transition-colors duration-200 hover:text-blue-800"
+                  className="inline-flex items-center text-primary transition-colors duration-200 hover:text-primary/80"
                 >
                   Czytaj więcej
                   <ArrowRight className="ml-2 h-4 w-4" />
                 </a>
-              </div>
-            </div>
+              </CardFooter>
+            </Card>
           ))}
         </div>
       </MaxWidthWrapper>
