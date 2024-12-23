eebd08cefc9560cc1d423fdb7e9d4e585bfa02f6
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-12-23 18:55:31 +0100

    Create projectCardData.ts

---
+++ src/lib/projectCardData.ts
@@ -0,0 +1,34 @@
+const projectCardData = [
+  {
+    id: 1,
+    title: "Konserwacja Baszty Ferbera",
+    location: "Wzgórze Katedralne we Fromborku",
+    timeframe: "Styczeń 2024 – Grudzień 2024",
+    description:
+      "Stabilizacja konstrukcji i usunięcie czynników powodujących destrukcję zabytkowej substancji. Prace obejmowały remont więźby dachowej, wymianę pokrycia oraz konserwację oryginalnego wątku ceglanego i detali kamiennych.",
+    imageUrl: "/basteja.webp",
+    slug: "konserwacja-baszty-ferbera",
+  },
+  {
+    id: 2,
+    title: "Konserwacja Kościoła w Rogowie",
+    location: "Rogowo, gmina Lubicz",
+    timeframe: "Od stycznia 2024 r.",
+    description:
+      "Jeden z najważniejszych kościołów bezwieżowych na terenie dawnego państwa krzyżackiego. Prace koncentrują się głównie na usunięciu przyczyn zawilgocenia oraz konserwacji wątku ceglanego i detali kamiennych.",
+    imageUrl: "/rogowo.webp",
+    slug: "konserwacja-kosciola-w-rogowie",
+  },
+  {
+    id: 3,
+    title: "Konserwacja Kościoła Farnego w Grudziądzu",
+    location: "Grudziądz, Rynek Starego Miasta",
+    timeframe: "Od 2018 r.",
+    description:
+      "Jedna z najstarszych świątyń miasta. Prace obejmują renowację gotyckich malowideł wewnątrz i zabytkowej elewacji, z celem przywrócenia strukturalnej stabilności i historycznego wyglądu.",
+    imageUrl: "/farnego.webp",
+    slug: "konserwacja-kosciola-farnego-w-grudziadzu",
+  },
+];
+
+export default projectCardData;
