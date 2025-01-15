2ad999cc44326366e54b95bebe52871623830dd9
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-01-15 14:28:09 +0100

    Update projectCardData.ts

--- src/lib/projectCardData.ts
+++ src/lib/projectCardData.ts
@@ -1,17 +1,17 @@
 const projectCardData = [
   {
     id: 1,
-    title: "Konserwacja Baszty Ferbera",
+    title: "Baszta Ferbera",
     location: "Wzgórze Katedralne we Fromborku",
     timeframe: "Styczeń 2024 – Grudzień 2024",
     description:
-      "Stabilizacja konstrukcji i usunięcie czynników powodujących destrukcję zabytkowej substancji. Prace obejmowały remont więźby dachowej, wymianę pokrycia oraz konserwację oryginalnego wątku ceglanego i detali kamiennych.",
+      "Baszta Ferbera jest jednym z najbardziej oryginalnych elementów fortyfikacji Wzgórza Katedralnego we Fromborku. Nasz zespół podjął się zadania konserwacji budowli, której stan ze względu na destrukcję materiałów z których jest wzniesiona wykluczał jej użytkowanie. Przeprowadzone w 2024 roku prace przywróciły walory estetyczne budynku oraz znacznie poprawiły jego stan techniczny, umożliwiając dalsze prace mające na celu ewentualną jego adaptację do nowych funkcji.",
     imageUrl: "/basteja.webp",
     slug: "konserwacja-baszty-ferbera",
   },
   {
     id: 2,
-    title: "Konserwacja Kościoła w Rogowie",
+    title: "Kościół w Rogowie",
     location: "Rogowo, gmina Lubicz",
     timeframe: "Od stycznia 2024 r.",
     description:
@@ -21,7 +21,7 @@ const projectCardData = [
   },
   {
     id: 3,
-    title: "Konserwacja Kościoła Farnego w Grudziądzu",
+    title: "Kościół Farny w Grudziądzu",
     location: "Grudziądz, Rynek Starego Miasta",
     timeframe: "Od 2018 r.",
     description:
