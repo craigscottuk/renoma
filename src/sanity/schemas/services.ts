316f3896ae18703db30b21d72baf30816e58c174
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-01-28 12:21:36 +0100

    Update services.ts

--- src/sanity/schemas/services.ts
+++ src/sanity/schemas/services.ts
@@ -92,6 +92,7 @@ export const servicesHeader = defineType({
           { title: "Pełna szerokość powyżej", value: "fullWidthAbove" },
           { title: "Pełna szerokość poniżej", value: "fullWidthBelow" },
           { title: "Portret po prawej", value: "portraitRight" },
+          { title: "Krajobraz po prawej", value: "landscapeRight" },
         ],
         layout: "radio",
       },
