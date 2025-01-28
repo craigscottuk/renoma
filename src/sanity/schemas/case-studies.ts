c990d3de0c1029baa36717d931e132ab8e083150
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-01-28 12:21:32 +0100

    Update case-studies.ts

--- src/sanity/schemas/case-studies.ts
+++ src/sanity/schemas/case-studies.ts
@@ -90,6 +90,7 @@ export const caseStudyHeader = defineType({
           { title: "Pełna szerokość powyżej", value: "fullWidthAbove" },
           { title: "Pełna szerokość poniżej", value: "fullWidthBelow" },
           { title: "Portret po prawej", value: "portraitRight" },
+          { title: "Krajobraz po prawej", value: "landscapeRight" },
         ],
         layout: "radio",
       },
