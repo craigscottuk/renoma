2589d61a81dd45f0154929dd591af6d1cd8bf3ec
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-04 15:58:14 +0100

    Update bibliography-section.tsx

--- src/app/[locale]/realizacje/[slug]/bibliography-section.tsx
+++ src/app/[locale]/realizacje/[slug]/bibliography-section.tsx
@@ -1,8 +1,10 @@
+"use client";
 // cSpell:disable
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { Separator } from "@/components/ui/separator";
 import { PortableText, PortableTextBlock } from "@portabletext/react";
 import { portableTextComponents } from "@/lib/portableTextComponents";
+import { FadeInSection } from "@/components/fade-in-section";
 
 interface BibliographySectionProps {
   title: string;
@@ -14,23 +16,25 @@ export default function BibliographySection({
   content,
 }: BibliographySectionProps) {
   return (
-    <section className="mb-16">
-      <MaxWidthWrapper>
-        <Separator className="mb-16" />
+    <FadeInSection>
+      <section className="mb-16">
+        <MaxWidthWrapper>
+          <Separator className="mb-16" />
 
-        <h2 className="mb-8 w-full max-w-[43rem] font-bolder text-[2rem] text-zinc-800">
-          {title}
-        </h2>
-      </MaxWidthWrapper>
+          <h2 className="mb-8 w-full max-w-[43rem] font-bolder text-[2rem] text-zinc-800">
+            {title}
+          </h2>
+        </MaxWidthWrapper>
 
-      <MaxWidthWrapper>
-        <div className="flex flex-col lg:w-4/5 lg:items-start">
-          <PortableText
-            value={content || []}
-            components={portableTextComponents}
-          />
-        </div>
-      </MaxWidthWrapper>
-    </section>
+        <MaxWidthWrapper>
+          <div className="flex flex-col lg:w-4/5 lg:items-start">
+            <PortableText
+              value={content || []}
+              components={portableTextComponents}
+            />
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </FadeInSection>
   );
 }
