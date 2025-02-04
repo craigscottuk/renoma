500e476aed843e2617d0c0a26be4d328510195b1
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-04 15:54:42 +0100

    Update our-history.tsx

--- src/app/[locale]/o-nas/our-history.tsx
+++ src/app/[locale]/o-nas/our-history.tsx
@@ -1,8 +1,10 @@
+"use client";
 // cSpell:disable
 import clsx from "clsx";
 import Timeline, { TimelineEvent } from "./timeline";
 import SectionTitle from "@/components/section-title";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { FadeInSection } from "@/components/fade-in-section";
 
 interface OurHistoryProps {
   title: string;
@@ -18,19 +20,21 @@ export default function OurHistory({
   return (
     <section className={clsx("mx-auto bg-white text-zinc-950/90", paddingY)}>
       <MaxWidthWrapper>
-        <SectionTitle
-          title={title}
-          as="h2"
-          motionPreset="blur-left"
-          textColor="black"
-          textAlign="center"
-        />
-        <div className="mx-auto mt-6 max-w-[46rem] text-center text-lg text-zinc-700">
-          Od skromnych początków do pozycji lidera w konserwacji zabytków –
-          nasza historia to lata wyzwań i sukcesów, które ukształtowały Renomę.
-          Zapraszamy do poznania kluczowych momentów, które zbudowały nasze
-          doświadczenie i zaufanie klientów.
-        </div>
+        <FadeInSection>
+          <SectionTitle
+            title={title}
+            as="h2"
+            motionPreset="blur-left"
+            textColor="black"
+            textAlign="center"
+          />
+          <div className="mx-auto mt-6 max-w-[46rem] text-center text-lg text-zinc-700">
+            Od skromnych początków do pozycji lidera w konserwacji zabytków –
+            nasza historia to lata wyzwań i sukcesów, które ukształtowały
+            Renomę. Zapraszamy do poznania kluczowych momentów, które zbudowały
+            nasze doświadczenie i zaufanie klientów.
+          </div>
+        </FadeInSection>
       </MaxWidthWrapper>
 
       <div className="mt-12 bg-zinc-100 p-16 pb-48">
