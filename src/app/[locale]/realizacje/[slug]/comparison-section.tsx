12de72d16f8212119f12af410ca58cbedde4aa67
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-04 15:58:14 +0100

    Update comparison-section.tsx

--- src/app/[locale]/realizacje/[slug]/comparison-section.tsx
+++ src/app/[locale]/realizacje/[slug]/comparison-section.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { Separator } from "@/components/ui/separator";
 import { CarouselComparison } from "./carousel-comparison";
 import { PortableText, PortableTextBlock } from "@portabletext/react";
 import { portableTextComponents } from "@/lib/portableTextComponents";
+import { FadeInSection } from "@/components/fade-in-section";
 
 interface ComparisonSectionProps {
   title: string;
@@ -26,35 +29,37 @@ export default function ComparisonSection({
   }
 
   return (
-    <section className="mb-16">
-      <MaxWidthWrapper>
-        <Separator className="mb-16" />
-        <h2 className="mb-8 w-full max-w-[43rem] font-bolder text-[2rem] text-zinc-800">
-          {title}
-        </h2>
-      </MaxWidthWrapper>
+    <FadeInSection>
+      <section className="mb-16">
+        <MaxWidthWrapper>
+          <Separator className="mb-16" />
+          <h2 className="mb-8 w-full max-w-[43rem] font-bolder text-[2rem] text-zinc-800">
+            {title}
+          </h2>
+        </MaxWidthWrapper>
 
-      <MaxWidthWrapper>
-        <div className="mb-20 flex flex-col lg:gap-24">
-          {content?.length > 0 && (
-            <div className="flex-1">
-              <div className="text-pretty text-[1.1rem] leading-[1.75] lg:max-w-[30rem]">
-                <PortableText
-                  value={content || []}
-                  components={portableTextComponents}
-                />
+        <MaxWidthWrapper>
+          <div className="mb-20 flex flex-col lg:gap-24">
+            {content?.length > 0 && (
+              <div className="flex-1">
+                <div className="text-pretty text-[1.1rem] leading-[1.75] lg:max-w-[30rem]">
+                  <PortableText
+                    value={content || []}
+                    components={portableTextComponents}
+                  />
+                </div>
               </div>
-            </div>
-          )}
-          {comparisons?.length > 0 && (
-            <div className="flex flex-col lg:w-4/5 lg:items-start">
-              <div className="w-full">
-                <CarouselComparison comparisons={comparisons} />
+            )}
+            {comparisons?.length > 0 && (
+              <div className="flex flex-col lg:w-4/5 lg:items-start">
+                <div className="w-full">
+                  <CarouselComparison comparisons={comparisons} />
+                </div>
               </div>
-            </div>
-          )}
-        </div>
-      </MaxWidthWrapper>
-    </section>
+            )}
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </FadeInSection>
   );
 }
