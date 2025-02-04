cfca4551280247650e3824beed805975ad25d22e
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-04 15:58:18 +0100

    Update what-we-offer.tsx

--- src/app/[locale]/ucz-sie-z-nami/what-we-offer.tsx
+++ src/app/[locale]/ucz-sie-z-nami/what-we-offer.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { CircleCheck } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import SectionTitle from "@/components/section-title";
 import clsx from "clsx";
+import { FadeInSection } from "@/components/fade-in-section";
 
 // Define the type for our feature items
 interface WhatWeOfferProps {
@@ -22,30 +25,30 @@ export default function WhatWeOffer({
   return (
     <section className={clsx("mx-auto bg-zinc-900 text-zinc-50", paddingY)}>
       <MaxWidthWrapper>
-        <SectionTitle title={title} className="mb-20" textColor="white" />
+        <FadeInSection translateY>
+          <SectionTitle title={title} className="mb-20" textColor="white" />
+        </FadeInSection>
       </MaxWidthWrapper>
       <MaxWidthWrapper>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
           {offers.map((feature, index) => (
-            <Card
-              key={index}
-              className="border-zinc-700 bg-zinc-800 text-zinc-100/90"
-            >
-              <CardHeader>
-                <div className="flex flex-col items-start">
-                  <CircleCheck className="mb-4 h-8 w-8 text-gold-900" />
-
-                  <CardTitle className="font-regular text-[1.7rem] leading-tight tracking-[-0.015em]">
-                    {feature.title}
-                  </CardTitle>
-                </div>
-              </CardHeader>
-              <CardContent>
-                <p className="text-[1.1rem] text-zinc-300">
-                  {feature.description}
-                </p>
-              </CardContent>
-            </Card>
+            <FadeInSection translateY key={index}>
+              <Card className="h-full border-zinc-700 bg-zinc-800 text-zinc-100/90">
+                <CardHeader>
+                  <div className="flex flex-col items-start">
+                    <CircleCheck className="mb-4 h-8 w-8 text-gold-900" />
+                    <CardTitle className="font-regular text-[1.7rem] leading-tight tracking-[-0.015em]">
+                      {feature.title}
+                    </CardTitle>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-[1.1rem] text-zinc-300">
+                    {feature.description}
+                  </p>
+                </CardContent>
+              </Card>
+            </FadeInSection>
           ))}
         </div>
       </MaxWidthWrapper>
