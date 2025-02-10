2c5c7a42a1f4c31f64d6bc7e29616ff1fdfd857f
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-10 19:12:31 +0100

    Create page.tsx

---
+++ src/app/[locale]/os-czasu/page.tsx
@@ -0,0 +1,165 @@
+// cSpell:disable
+// src/app/[locale]/o-nas/page.tsx
+// import { AboutUs } from "./about-us";
+import OurHistory from "./our-history";
+import { client } from "@/sanity/client";
+import { PortableTextBlock } from "next-sanity";
+// import PageHeader from "@/components/page-header";
+import { getTranslations } from "next-intl/server";
+import { setRequestLocale } from "next-intl/server";
+// import { ctaContent } from "@/lib/ctaContent";
+// import CTA from "@/components/cta";
+
+const QUERY = `
+{
+ "aboutUsHeader": *[_type == "aboutUsHeader"][0]{
+    "label": coalesce(label[_key == $locale][0].value, "Brak tłumaczenia"),
+    "title": coalesce(title[_key == $locale][0].value, "Brak tłumaczenia"),
+    "description": coalesce(description[_key == $locale][0].value, "Brak tłumaczenia"),
+    "image": image,
+    "imageAlt": coalesce(image.alt[_key == $locale][0].value, "Brak tłumaczenia"),
+    "imageLayout": imageLayout,
+    "backgroundColor": backgroundColor
+  },
+
+  "aboutUs": *[_type == "aboutUs"][0]{
+    "title": coalesce(title[_key == $locale][0].value, "Brak tłumaczenia"),
+     "text": coalesce(text[$locale], []),
+  },
+
+  "ourHistory": *[_type == "ourHistory"][0]{
+    "title": coalesce(title[_key == $locale][0].value, "Brak tłumaczenia"),
+     "text": coalesce(text[_key == $locale][0].value, "Brak tłumaczenia"),
+    "timeline": timeline[]{
+      "year": year,
+      "content": coalesce(content[$locale], []),
+      "images": images[]{
+        "src": src.asset->url,
+        "caption": coalesce(caption[_key == $locale][0].value, ""),
+        "aspectRatio": aspectRatio
+      }
+    }
+  }
+}
+`;
+
+const OPTIONS = { next: { revalidate: 10 } };
+//86400
+
+type Props = {
+  params: { locale: string };
+};
+
+interface Content {
+  aboutUsHeader: {
+    label: string;
+    title: string;
+    description: string;
+    image?: string;
+    imageAlt?: string;
+    imageLayout?:
+      | "fullWidthAbove"
+      | "fullWidthBelow"
+      | "portraitRight"
+      | "landscapeRight"
+      | "noImage";
+    backgroundColor?: "black" | "white";
+  };
+  aboutUs: {
+    title: string;
+    text: PortableTextBlock[];
+  };
+  ourHistory: {
+    title: string;
+    text: string;
+    timeline: TimelineItem[];
+  };
+}
+
+interface TimelineItem {
+  title: string;
+  year: string;
+  content: PortableTextBlock[];
+  images?: TimelineImage[];
+}
+
+interface TimelineImage {
+  src: string;
+  caption: string;
+  aspectRatio?: "none" | "landscape" | "portrait" | "square";
+}
+
+export async function generateMetadata({ params: { locale } }: Props) {
+  const t = await getTranslations({ locale, namespace: "metadata" });
+
+  return {
+    title: t("about.title"),
+    description: t("about.description"),
+    openGraph: {
+      title: t("about.title"),
+      description: t("about.description"),
+    },
+    twitter: {
+      title: t("about.title"),
+      description: t("about.description"),
+    },
+  };
+}
+
+export default async function OsCzasu({ params: { locale } }: Props) {
+  // Set the locale for static generation
+  setRequestLocale(locale);
+
+  // Fetch localized content from Sanity using locale from params
+  const content = await client.fetch<Content>(QUERY, { locale }, OPTIONS);
+
+  const {
+    //  aboutUsHeader,
+    //  aboutUs,
+    ourHistory,
+  } = content;
+
+  // console.log(" content", aboutUsHeader);
+
+  return (
+    <>
+      {/* Page Header for About Us */}
+      {/* {aboutUsHeader && (
+        <PageHeader
+          label={aboutUsHeader.label}
+          title={aboutUsHeader.title}
+          description={aboutUsHeader.description}
+          image={aboutUsHeader.image}
+          imageAlt={aboutUsHeader.imageAlt}
+          imageLayout={aboutUsHeader.imageLayout}
+          backgroundColor={aboutUsHeader.backgroundColor}
+        />
+      )} */}
+
+      {/* About Us, Our Values section */}
+      {/* {aboutUs && (
+        <AboutUs
+          title={aboutUs.title}
+          text={aboutUs.text}
+          paddingY="py-20 md:py-48"
+        />
+      )} */}
+
+      {/* Interactive Timeline component */}
+      {ourHistory && (
+        <OurHistory
+          title={ourHistory.title}
+          text={ourHistory.text}
+          events={ourHistory.timeline}
+          paddingY="pt-20 md:pt-48"
+        />
+      )}
+
+      {/* <CTA
+        title={ctaContent.title}
+        description={ctaContent.description}
+        buttonText={ctaContent.buttonText}
+      /> */}
+    </>
+  );
+}
