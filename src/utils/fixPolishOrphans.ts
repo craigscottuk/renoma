ff279761a6da9251d04bc98c84c99842510ba52c
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-11 16:41:08 +0100

    Update fixPolishOrphans.ts

--- src/utils/fixPolishOrphans.ts
+++ src/utils/fixPolishOrphans.ts
@@ -1,9 +1,15 @@
+// Fixes Polish orphans by replacing specific spaces with non-breaking spaces.
 export default function fixPolishOrphans(text: string) {
   const shortWords = ["z", "w", "i", "na", "o"];
   let fixedText = text;
-  shortWords.forEach((w) => {
-    const re = new RegExp(`\\s${w}\\s`, "gi");
-    fixedText = fixedText.replace(re, ` ${w}\u00A0`);
+  shortWords.forEach((word) => {
+    const capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
+    const re = new RegExp(`\\s(${word}|${capitalizedWord})\\s`, "g");
+    fixedText = fixedText.replace(re, (match, p1) => {
+      return p1.charAt(0) === p1.charAt(0).toUpperCase()
+        ? ` ${capitalizedWord}\u00A0`
+        : ` ${word}\u00A0`;
+    });
   });
   return fixedText;
 }
