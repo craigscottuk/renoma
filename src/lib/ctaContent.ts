1197b459e2f94e09b7ff50974f093c674e9d0f1c
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-11 16:41:04 +0100

    Delete ctaContent.ts

--- src/lib/ctaContent.ts
+++
@@ -1,8 +0,0 @@
-export const ctaContent = {
-  title: "Skonsultuj Swój Projekt",
-  description: `Nasza firma oferuje kompleksową konsultację dla projektów
-                renowacyjnych i konserwatorskich. Skontaktuj się z nami, aby
-                omówić swoje potrzeby i uzyskać spersonalizowane wsparcie na
-                każdym etapie realizacji projektu.`,
-  buttonText: "SKONSULTUJ",
-};
