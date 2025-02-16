006935eb6bc1f25926de3ea54e82b2ea5b9fd67a
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-16 21:02:06 +0100

    Update index.ts

--- src/sanity/schemas/index.ts
+++ src/sanity/schemas/index.ts
@@ -7,33 +7,57 @@ import {
   aboutSectionHome,
   servicesSectionHome,
   logoSectionHome,
+  homePageSeo,
 } from "./home";
 import {
   about,
   aboutUsHeader,
   ourHistory,
   aboutUs,
-  aboutPageMetadata,
+  aboutPageSeo,
 } from "./about";
 import {
   servicesList,
   servicesHeader,
   services,
   servicesGroup,
+  servicesPageSeo,
 } from "./services";
 import { caseStudyEntry } from "./case-study-entry";
-import { caseStudyHeader, caseStudies } from "./case-studies";
-import { aboutLab, labOffer, renomaLab, renomaLabHeader } from "./renomaLab";
+import {
+  caseStudyHeader,
+  caseStudies,
+  caseStudiesPageSeo,
+} from "./case-studies";
+import {
+  aboutLab,
+  labOffer,
+  renomaLab,
+  renomaLabHeader,
+  renomaLabPageSeo,
+} from "./renomaLab";
 import {
   learnWithUsHeader,
   learnWithUs,
   whatWeOffer,
   whoWeAreLookingFor,
+  learnWithUsPageSeo,
 } from "./learn-with-us";
-import { jobOffers, workWithUs, workWithUsHeader } from "./work-with-us";
-import { contact, contactHeader, contactForm, contactDetails } from "./contact";
+import {
+  jobOffers,
+  workWithUs,
+  workWithUsHeader,
+  workWithUsPageSeo,
+} from "./work-with-us";
+import {
+  contact,
+  contactHeader,
+  contactForm,
+  contactDetails,
+  contactPageSeo,
+} from "./contact";
 import { settings } from "./settings";
-import { privacy, privacyHeader, privacyText } from "./privacy";
+import { privacy, privacyHeader, privacyText, privacyPageSeo } from "./privacy";
 import {
   basicText,
   portableTextWithHeadings,
@@ -42,7 +66,7 @@ import {
 import { sectionContent } from "./sectionContent";
 import { cta, ctaContent } from "./cta";
 import { socialMedia, socialMediaLinks } from "./socialMedia";
-import { faq, faqHeader, faqList } from "./faq";
+import { faq, faqHeader, faqList, faqPageSeo } from "./faq";
 
 // Export schema configuration
 export const schema: { types: SchemaTypeDefinition[] } = {
@@ -53,23 +77,26 @@ export const schema: { types: SchemaTypeDefinition[] } = {
     aboutSectionHome,
     servicesSectionHome,
     logoSectionHome,
+    homePageSeo,
 
     // O nas
     about,
     aboutUsHeader,
     aboutUs,
     ourHistory,
-    aboutPageMetadata,
+    aboutPageSeo,
 
     // Usługi
     services,
     servicesHeader,
     servicesGroup,
     servicesList,
+    servicesPageSeo,
 
     // Realizacje
     caseStudies,
     caseStudyHeader,
+    caseStudiesPageSeo,
 
     // Wpis realizacji
     caseStudyEntry,
@@ -79,31 +106,35 @@ export const schema: { types: SchemaTypeDefinition[] } = {
     renomaLabHeader,
     aboutLab,
     labOffer,
+    renomaLabPageSeo,
 
     // Ucz się z nami
     learnWithUs,
     learnWithUsHeader,
     whatWeOffer,
     whoWeAreLookingFor,
+    learnWithUsPageSeo,
 
     // Pracuj z nami
     workWithUs,
     workWithUsHeader,
     jobOffers,
+    workWithUsPageSeo,
 
     // FAQ
     faq,
     faqHeader,
     faqList,
+    faqPageSeo,
 
     // Kontakt
     contact,
     contactHeader,
     contactForm,
     contactDetails,
+    contactPageSeo,
 
     // CTA
-
     cta,
     ctaContent,
 
@@ -111,6 +142,7 @@ export const schema: { types: SchemaTypeDefinition[] } = {
     privacy,
     privacyHeader,
     privacyText,
+    privacyPageSeo,
 
     // Ustawienia
     settings,
