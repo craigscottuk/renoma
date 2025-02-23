7fc8b84d9a0d2de9ebe1ad4c2a00e31f3a91f49b
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-23 19:14:28 +0100

    Update project-card.tsx

--- src/app/[locale]/realizacje/project-card.tsx
+++ src/app/[locale]/realizacje/project-card.tsx
@@ -4,12 +4,13 @@ import MaxWidthWrapper from "@/components/max-width-wrapper";
 import {
   Card,
   // CardContent,
-  CardFooter,
+  // CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import clsx from "clsx";
 import { FadeInSection } from "@/components/fade-in-section";
+import fixPolishOrphans from "@/utils/fixPolishOrphans";
 // import fixPolishOrphans from "@/utils/fixPolishOrphans";
 
 interface ProjectCardProps {
@@ -68,14 +69,14 @@ export default function ProjectCard({
       className={clsx("bg-white", paddingY, selectedColorScheme.section)}
     >
       <MaxWidthWrapper>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="grid grid-cols-1 items-stretch gap-x-6 gap-y-24 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
           {projectCardData
             .filter((project) => !project.draft)
             .map((project, index) => (
               <FadeInSection key={index} translateY>
                 <Card
                   className={clsx(
-                    "flex flex-col overflow-hidden",
+                    "flex h-full flex-col overflow-hidden border border-none shadow-none",
                     selectedColorScheme.card,
                   )}
                 >
@@ -91,10 +92,11 @@ export default function ProjectCard({
                   </div>
                   <CardHeader>
                     <div className="flex items-center space-x-2">
-                      <CardTitle className="text-[1.7rem] leading-tight tracking-[-0.015em] text-zinc-800">
+                      <CardTitle className="font-bolder text-[1.25rem] leading-snug tracking-[-0.010em] text-zinc-800">
                         <h2>
                           {/* <a href={`/realizacje/${project.slug}`}> */}
-                          {project.title}
+
+                          {fixPolishOrphans(project.title)}
                           {/* </a> */}
                         </h2>
                       </CardTitle>
@@ -122,14 +124,14 @@ export default function ProjectCard({
                       </p>
                     )}
                   </CardContent> */}
-                  <CardFooter>
-                    {/* <AnimatedLink
+                  {/* <CardFooter> */}
+                  {/* <AnimatedLink
                       className="text-`zinc-900 text-base"
                       href={`/realizacje/${project.slug}`}
                     >
                       Zobacz cały projekt
                     </AnimatedLink> */}
-                  </CardFooter>
+                  {/* </CardFooter> */}
                 </Card>
               </FadeInSection>
             ))}
