13153052a44390cd7535f4b5316a43d1ff02d78f
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-25 16:51:33 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 export const dynamic = "force-dynamic";
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
-  redirect("/");
+  redirect("/pl");
 }
