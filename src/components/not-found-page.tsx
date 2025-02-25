6819b0de0fb4328216e21cfb57ccd95cb6104ff7
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-25 18:43:23 +0100

    Update not-found-page.tsx

--- src/components/not-found-page.tsx
+++ src/components/not-found-page.tsx
@@ -1,15 +1,19 @@
-import { Link } from '@/i18n/routing';
-import { useTranslations } from 'next-intl';
+// import { Link } from '@/i18n/routing';
+import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 export default function NotFound() {
-  const t = useTranslations('not-found-page');
+  const t = useTranslations("not-found-page");
 
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center p-8 text-center'>
-      <h1 className='mb-4 text-4xl font-bold'>{t('title')}</h1>
-      <p className='mb-8 text-lg'>{t('description')}</p>
-      <Link href='/' className='text-blue-500 hover:underline'>
-        {t('goBackHome')}
+    <div className="flex min-h-screen flex-col items-center justify-center p-8 text-center">
+      <h1 className="mb-4 text-4xl font-bold">{t("title")}</h1>
+      <p className="mb-8 text-lg">{t("description")}</p>
+
+      {/* TODO */}
+      {/* <Link href='/' className='text-blue-500 hover:underline'> */}
+      <Link href="/pl" className="text-blue-500 hover:underline">
+        {t("goBackHome")}
       </Link>
     </div>
   );
