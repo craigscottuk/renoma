845aed7816933b367f4a35270a5e6948831365d4
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-02-27 18:07:09 +0100

    Update base-layout.tsx

--- src/components/base-layout.tsx
+++ src/components/base-layout.tsx
@@ -1,12 +1,11 @@
 import { clsx } from "clsx";
-
 import Header from "./header";
 import { ReactNode } from "react";
 import localFont from "next/font/local";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
 import Footer from "@/components/sections-footer/footer";
-// import { Analytics } from "@vercel/analytics/react";
+import { client } from "@/sanity/client";
 
 const helveticaNeueLight = localFont({
   src: "./fonts/HelveticaNeueLight.otf",
@@ -23,6 +22,38 @@ const helveticaNeueMedium = localFont({
   variable: "--font-helvetica-neue-medium",
 });
 
+const QUERY = `
+{
+  "serviceGroups": *[_type == "servicesGroup"][0]{
+    "serviceGroupOne": {
+      "title": coalesce(serviceGroupOne.title, "Brak tłumaczenia"),
+      "services": serviceGroupOne.services[]{
+        "title": coalesce(title[_key == $locale][0].value, "Brak tłumaczenia")
+      }
+    },
+    "serviceGroupTwo": {
+      "title": coalesce(serviceGroupTwo.title, "Brak tłumaczenia"),
+      "services": serviceGroupTwo.services[]{
+        "title": coalesce(title[_key == $locale][0].value, "Brak tłumaczenia")
+      }
+    },
+    "serviceGroupThree": {
+      "title": coalesce(serviceGroupThree.title, "Brak tłumaczenia"),
+      "services": serviceGroupThree.services[]{
+        "title": coalesce(title[_key == $locale][0].value, "Brak tłumaczenia")
+      }
+    }
+  },
+  "socialMediaLinks": *[_type == "socialMediaLinks"][0]{
+    "facebook": coalesce(facebook, ""),
+    "instagram": coalesce(instagram, ""),
+    "linkedIn": coalesce(linkedIn, "")
+  }
+}
+`;
+
+const OPTIONS = { next: { revalidate: 86400 } };
+
 type Props = {
   children: ReactNode;
   locale: string;
@@ -35,6 +66,13 @@ export default async function BaseLayout({ children, locale }: Props) {
   } catch (error) {
     console.error("Error loading messages:", error);
   }
+
+  const footerData = await client.fetch(QUERY, { locale }, OPTIONS);
+  const serviceGroups = [
+    footerData.serviceGroups.serviceGroupOne,
+    footerData.serviceGroups.serviceGroupTwo,
+    footerData.serviceGroups.serviceGroupThree,
+  ];
 
   return (
     <html className="h-full" lang={locale}>
@@ -50,7 +88,11 @@ export default async function BaseLayout({ children, locale }: Props) {
           <div className="flex min-h-screen flex-col">
             <Header />
             <main className="flex-1">{children}</main>
-            <Footer locale={locale} />
+            <Footer
+              locale={locale}
+              serviceGroups={serviceGroups}
+              socialMediaLinks={footerData.socialMediaLinks}
+            />
           </div>
         </NextIntlClientProvider>
         {/* <Analytics /> */}
