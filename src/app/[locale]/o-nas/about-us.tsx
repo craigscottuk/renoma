5ec45a7549c996568ba12aab16be4349fecc07a1
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-06 20:42:07 +0100

    Update about-us.tsx

--- src/app/[locale]/o-nas/about-us.tsx
+++ src/app/[locale]/o-nas/about-us.tsx
@@ -21,9 +21,9 @@ export function AboutUs({ title, text, paddingY }: AboutUsProps) {
     <section className={clsx("mx-auto bg-zinc-200 text-zinc-950/90", paddingY)}>
       <MaxWidthWrapper>
         <FadeInSection>
-          <div className="mr-10 grid grid-cols-1 gap-8 md:mr-20 lg:mr-0 lg:grid-cols-3 lg:gap-20">
+          <div className="mr-10 grid grid-cols-1 gap-8 md:mr-20 lg:mr-0 lg:grid-cols-3 lg:gap-20 xl:gap-20">
             <SectionTitle title={title} textColor="black" />
-            <div className="lg:col-span-2 lg:col-start-2 lg:columns-2 lg:gap-20">
+            <div className="lg:col-span-2 lg:col-start-2 lg:columns-2 lg:gap-8 xl:gap-20">
               <PortableText
                 value={newText}
                 components={portableTextComponents}
