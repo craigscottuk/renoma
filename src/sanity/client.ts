8e0d5d44a0226fd703d334852027104f7b0f5034
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-08 16:26:00 +0100

    Update client.ts

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,4 +1,7 @@
-import { createClient } from "next-sanity";
+//src/sanity/client.ts
+// cSpell:disable
+
+import { createClient, type QueryParams } from "next-sanity";
 
 export const client = createClient({
   projectId: "6ivsfkzi",
@@ -6,3 +9,30 @@ export const client = createClient({
   apiVersion: "2024-01-01",
   useCdn: false,
 });
+
+/**
+ * The helper function that merges time-based + tag-based revalidation
+ *
+ * If you pass `tags: []`, it uses time-based revalidation (defaults to 60s).
+ * If you pass `tags: ["someTag"]`, it sets revalidate=false and uses the tag-based approach.
+ */
+export async function sanityFetch<QueryResponse>({
+  query,
+  params = {},
+  revalidate = 86400, // 86400
+  tags = [],
+}: {
+  query: string;
+  params?: QueryParams;
+  revalidate?: number | false;
+  tags?: string[];
+}): Promise<QueryResponse> {
+  return client.fetch<QueryResponse>(query, params, {
+    next: {
+      // If tags are present, we override time-based revalidation
+      // and rely purely on on-demand revalidation for those tags.
+      revalidate: tags.length ? false : revalidate,
+      tags,
+    },
+  });
+}
