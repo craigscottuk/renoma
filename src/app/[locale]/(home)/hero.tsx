44441acb22e65c829bc63df757a9831494fb9736
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-23 22:23:18 +0100

    Update hero.tsx

--- src/app/[locale]/(home)/hero.tsx
+++ src/app/[locale]/(home)/hero.tsx
@@ -2,7 +2,7 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import CustomButton from "@/components/ui/custom-button";
 import SectionTitle from "@/components/section-title";
-import MuxPlayer from "@mux/mux-player-react";
+// import MuxPlayer from "@mux/mux-player-react";
 
 interface HeroSectionProps {
   title: string;
@@ -14,7 +14,7 @@ export default function HeroSection({ title, sectionCTA }: HeroSectionProps) {
     <div className="relative mt-20 min-h-[calc(40vh)] md:mt-24 md:min-h-[calc(60vh)] lg:min-h-[calc(100vh-96px)]">
       {/* Background Video */}
 
-      <MuxPlayer
+      {/* <MuxPlayer
         streamType="on-demand"
         playbackId="xgOUTdWiGS2xzXqzdu02003z2Q01ILmhBPPk802mW21t2JM"
         metadataVideoTitle="Renoma"
@@ -23,15 +23,15 @@ export default function HeroSection({ title, sectionCTA }: HeroSectionProps) {
         loop={true}
         muted={true}
         autoPlay={true}
-      />
+      /> */}
 
       {/* Overlay for better readability */}
       <div className="absolute inset-0 bg-zinc-900 bg-opacity-25"></div>
 
-      {/* Hero Content */}
+      {/* HeroSection Content */}
       <div className="absolute inset-0 flex items-center">
         <MaxWidthWrapper>
-          {/* Hero Title */}
+          {/* HeroSection Title */}
           <SectionTitle
             title={title}
             as="h1"
