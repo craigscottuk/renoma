d1898b6a618568b9d3b9e4b1ce21af79078f5574
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-23 22:23:43 +0100

    Update job-offer.tsx

--- src/app/[locale]/pracuj-z-nami/job-offer.tsx
+++ src/app/[locale]/pracuj-z-nami/job-offer.tsx
@@ -175,7 +175,7 @@ export default function JobOfferCard({ job }: { job: JobOffer }) {
         </CardContent>
         <CardFooter className="flex items-center justify-between">
           <Button
-            className="bg-zinc-900 text-zinc-50 hover:bg-zinc-800 hover:text-zinc-100"
+            className="bg-zinc-50 text-zinc-950 hover:bg-zinc-800 hover:text-zinc-100"
             variant="outline"
             onClick={(e) => {
               e.stopPropagation();
