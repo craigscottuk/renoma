7c6f4dce2feddc650fd7aede0ee5b12c6f2ac72f
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-23 22:23:44 +0100

    Update bibliography-section.tsx

--- src/app/[locale]/realizacje/[slug]/bibliography-section.tsx
+++ src/app/[locale]/realizacje/[slug]/bibliography-section.tsx
@@ -16,7 +16,7 @@ export default function BibliographySection({
   title,
   content,
 }: BibliographySectionProps) {
-  // Apply transformPortableTextBlocks to portableTextBlock before rendering to fix Polish orphans on the end of each line.
+  // Transform PortableText blocks to fix Polish orphans of each line before rendering.
   const newContent = transformPortableTextBlocks(content);
 
   return (
