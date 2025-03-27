685089f07d0abf52ee94a706e8cbf16acae9329b
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-27 09:36:55 +0100

    Update page.tsx

--- src/app/[locale]/uslugi/page.tsx
+++ src/app/[locale]/uslugi/page.tsx
@@ -89,7 +89,6 @@ interface Content {
     imageAlt?: string;
     imageLayout?:
       | "fullWidthAbove"
-      | "fullWidthBelow"
       | "portraitRight"
       | "landscapeRight"
       | "noImage";
