4638cad9f8b953ef955ee12b02ec8c3e19ce888f
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-27 11:39:10 +0100

    Update page.tsx

--- src/app/[locale]/uslugi/page.tsx
+++ src/app/[locale]/uslugi/page.tsx
@@ -188,7 +188,7 @@ export async function generateMetadata({ params: { locale } }: Props) {
     query: QUERY,
     params: { locale },
     tags: ["services"],
-    revalidate: 60, // 604800
+    revalidate: 604800, // 604800
   });
 
   return {
@@ -217,7 +217,7 @@ export default async function ONas({ params: { locale } }: Props) {
     query: QUERY,
     params: { locale },
     tags: ["services"],
-    revalidate: 60, // 604800
+    revalidate: 604800, // 604800
   });
 
   const { servicesHeader, servicesGroup, ctaContent } = content;
