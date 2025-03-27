6a9107472ccedefc20415b275b493c1718be9172
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-27 11:09:01 +0100

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -24,53 +24,55 @@ export default function Header({ socialMediaLinks }: HeaderProps) {
   // Get the isOpen, openMobileNav, and closeMobileNav functions from context
   const { isOpen, openMobileNav, closeMobileNav } = useMobileNav();
   return (
-    <header className="motion-preset-focus-md fixed left-0 z-30 h-20 w-[100vw] bg-white md:h-24">
-      <MaxWidthWrapper className="h-full">
-        <div className="flex h-full w-full items-center md:pb-0">
-          {/* Logo */}
-          <Link href="/">
-            <img
-              className="h-[45px] lg:motion-preset-blur-up-sm md:h-[55px] lg:h-[65px]"
-              src="/renoma-logo.svg"
-              alt="Renoma PKZ"
-            />
-          </Link>
+    <>
+      <header className="motion-preset-focus-md fixed left-0 z-30 h-20 w-[100vw] bg-white md:h-24">
+        <MaxWidthWrapper className="h-full">
+          <div className="flex h-full w-full items-center md:pb-0">
+            {/* Logo */}
+            <Link href="/">
+              <img
+                className="h-[45px] lg:motion-preset-blur-up-sm md:h-[55px] lg:h-[65px]"
+                src="/renoma-logo.svg"
+                alt="Renoma PKZ"
+              />
+            </Link>
 
-          {/* Nav Items */}
-          <div className="ml-auto flex h-full items-center pt-[0.8rem] md:pt-2.5 lg:pt-6">
-            <MainNavigation />
+            {/* Nav Items */}
+            <div className="ml-auto flex h-full items-center pt-[0.8rem] md:pt-2.5 lg:pt-6">
+              <MainNavigation />
 
-            {/* Locale Switcher */}
-            <LocaleSwitcher className="motion-preset-blur-up-sm ml-1 lg:ml-0.5 xl:ml-4" />
+              {/* Locale Switcher */}
+              <LocaleSwitcher className="motion-preset-blur-up-sm ml-1 lg:ml-0.5 xl:ml-4" />
 
-            {/* Contact */}
-            <Link href="/kontakt">
+              {/* Contact */}
+              <Link href="/kontakt">
+                <Button
+                  variant="ghost"
+                  className="px-1.5 sm:px-2 lg:hidden"
+                  aria-label={t("contactAriaLabel")}
+                >
+                  <Mail className="mr-0.5 min-h-5 min-w-5 stroke-1 text-zinc-950 sm:min-h-6 sm:min-w-6" />
+                </Button>
+              </Link>
+
+              {/* Mobile menu */}
               <Button
                 variant="ghost"
                 className="px-1.5 sm:px-2 lg:hidden"
-                aria-label={t("contactAriaLabel")}
+                onClick={openMobileNav}
+                aria-label={t("openMenuAriaLabel")}
               >
-                <Mail className="mr-0.5 min-h-5 min-w-5 stroke-1 text-zinc-950 sm:min-h-6 sm:min-w-6" />
+                <Menu className="min-h-7 min-w-7 stroke-1 text-zinc-950" />
               </Button>
-            </Link>
-
-            {/* Mobile menu */}
-            <Button
-              variant="ghost"
-              className="px-1.5 sm:px-2 lg:hidden"
-              onClick={openMobileNav}
-              aria-label={t("openMenuAriaLabel")}
-            >
-              <Menu className="min-h-7 min-w-7 stroke-1 text-zinc-950" />
-            </Button>
+            </div>
           </div>
-        </div>
-      </MaxWidthWrapper>
+        </MaxWidthWrapper>
+      </header>
       <MobileNav
         isOpen={isOpen}
         onClose={closeMobileNav}
         socialMediaLinks={socialMediaLinks}
       />
-    </header>
+    </>
   );
 }
