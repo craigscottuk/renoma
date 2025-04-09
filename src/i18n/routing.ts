78bf57c78b77a34f1245f0ac1ac38dad903dd085
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-04-09 11:59:09 +0200

    Update routing.ts

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,58 +4,60 @@ import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["pl", "en", "de"],
+  // TO RESTORE OTHER LANGUAGES: Uncomment 'en', 'de' in the following line
+  locales: ["pl"], // ["pl", "en", "de"]
   defaultLocale: "pl",
   pathnames: {
     "/": "/",
     "/o-nas": {
-      en: "/about-us",
-      de: "/uber-uns",
+      // TO RESTORE OTHER LANGUAGES: Uncomment these blocks
+      // en: "/about-us",
+      // de: "/uber-uns",
       pl: "/o-nas",
     },
     "/uslugi": {
-      en: "/services",
-      de: "/dienstleistungen",
+      // en: "/services",
+      // de: "/dienstleistungen",
       pl: "/uslugi",
     },
     "/realizacje": {
-      en: "/projects",
-      de: "/projekte",
+      // en: "/projects",
+      // de: "/projekte",
       pl: "/realizacje",
     },
     "/renoma-lab": {
-      en: "/renoma-lab",
-      de: "/renoma-lab",
+      // en: "/renoma-lab",
+      // de: "/renoma-lab",
       pl: "/renoma-lab",
     },
     "/ucz-sie-z-nami": {
-      en: "/learn-with-us",
-      de: "/lernen-sie-mit-uns",
+      // en: "/learn-with-us",
+      // de: "/lernen-sie-mit-uns",
       pl: "/ucz-sie-z-nami",
     },
     "/pracuj-z-nami": {
-      en: "/work-with-us",
-      de: "/arbeiten-sie-mit-uns",
+      // en: "/work-with-us",
+      // de: "/arbeiten-sie-mit-uns",
       pl: "/pracuj-z-nami",
     },
     "/kontakt": {
-      en: "/contact",
-      de: "/kontakt",
+      // en: "/contact",
+      // de: "/kontakt",
       pl: "/kontakt",
     },
     "/polityka-prywatnosci": {
-      en: "/privacy-policy",
-      de: "/datenschutzrichtlinie",
+      // en: "/privacy-policy",
+      // de: "/datenschutzrichtlinie",
       pl: "/polityka-prywatnosci",
     },
     "/realizacje/[slug]": {
-      en: "/projects/[slug]",
-      de: "/projekte/[slug]",
+      // en: "/projects/[slug]",
+      // de: "/projekte/[slug]",
       pl: "/realizacje/[slug]",
     },
     "/faq": {
-      en: "/faq",
-      de: "/faq",
+      // en: "/faq",
+      // de: "/faq",
       pl: "/faq",
     },
   },
