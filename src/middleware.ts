86ab7138429095c22a0ffa33b0b28f8a3f5529ff
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-05-09 11:04:17 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,8 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: [
-    // Enable a redirect to a matching locale at the root
-    "/",
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    "/(de|en|pl)/:path*",
-
-    // Enable locale redirects for everything EXCEPT /api, /admin, /_next, /_vercel, or files
-    "/((?!api|_next|_vercel|admin|.*\\..*|sitemap\\.xml|robots\\.txt).*)",
-  ],
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
